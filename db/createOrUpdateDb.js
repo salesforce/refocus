a06fb15ee4fef32bd0a0f88c9d3a803522c388e6
meynet-salesforce <c.meynet@salesforce.com>
2016-09-22 09:43:31 -0700

    Adding headers

--- db/createOrUpdateDb.js
+++ db/createOrUpdateDb.js
@@ -1,4 +1,12 @@
 /**
+ * Copyright (c) 2016, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
+
+/**
  * ./db/createOrUpdateDb.js
  *
  * If database and table schema already exist, just execute migratedb to
--- view/perspective/lensUtils.js
+++ view/perspective/lensUtils.js
@@ -1,4 +1,12 @@
 /**
+ * Copyright (c) 2016, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
+
+/**
  * DO NOT MODIFY THIS FILE. WE CANNOT GUARANTEE THAT YOUR LENS WILL WORK ONCE
  * IT IS INSTALLED INTO A REFOCUS INSTALLATION.
  */
