7a28f7f4ecd0e8d73adca82a058bf1395c092968
Shriram <sshankar@salesforce.com>
2016-12-17 00:39:12 -0800

    record level permissions: added GET /v1/[subjects|aspects|perspectives|lenses]/{key}/writers and added GET /v1/[subjects|aspects|perspectives|lenses]/{key}/writers/{userNameOrId} endpoints

--- api/v1/controllers/subjects.js
+++ api/v1/controllers/subjects.js
@@ -12,6 +12,7 @@
 'use strict';
 
 const helper = require('../helpers/nouns/subjects');
+const userProps = require('../helpers/nouns/users');
 const doDelete = require('../helpers/verbs/doDelete');
 const doFind = require('../helpers/verbs/doFind');
 const doGet = require('../helpers/verbs/doGet');
@@ -113,6 +114,55 @@ module.exports = {
     })
     .catch((err) => u.handleError(next, err, helper.modelName));
   }, // getSubjectHierarchy
+
+  /**
+   * GET /subjects/{key}/writers
+   *
+   * Retrieves all the writers associated with the aspect
+   *
+   * @param {IncomingMessage} req - The request object
+   * @param {ServerResponse} res - The response object
+   * @param {Function} next - The next middleware function in the stack
+   */
+  getSubjectWriters(req, res, next) {
+    const params = req.swagger.params;
+    const options = {};
+    options.scope = userProps.scopeMap.withoutSensitiveInfo;
+    u.findAssociatedInstances(helper,
+      params, helper.userModelAssociationName, options)
+    .then((o) => {
+      const retval = u.responsify(o, helper, req.method);
+      res.status(httpStatus.OK).json(retval);
+    })
+    .catch((err) => u.handleError(next, err, helper.modelName));
+  }, // getSubjectWriters
+
+  /**
+   * GET /subjects/{key}/writers/userNameOrId
+   *
+   * Determine whether a user is an authorized writer for a subject and returns
+   * the user record if so.
+   *
+   * @param {IncomingMessage} req - The request object
+   * @param {ServerResponse} res - The response object
+   * @param {Function} next - The next middleware function in the stack
+   */
+  getSubjectWriter(req, res, next) {
+    const params = req.swagger.params;
+    const options = {};
+    options.scope = userProps.scopeMap.withoutSensitiveInfo;
+    options.where = u.whereClauseForNameOrId(params.userNameOrId.value);
+    u.findAssociatedInstances(helper,
+      params, helper.userModelAssociationName, options)
+    .then((o) => {
+    // if the resolved object is an empty array, throw a ResourceNotFound error
+      u.throwErrorForEmptyArray(o,
+        params.userNameOrId.value, userProps.modelName);
+      const retval = u.responsify(o, helper, req.method);
+      res.status(httpStatus.OK).json(retval);
+    })
+    .catch((err) => u.handleError(next, err, helper.modelName));
+  }, // getSubjectWriter
 
   /**
    * PATCH /subjects/{key}
--- tests/testUtils.js
+++ tests/testUtils.js
@@ -12,11 +12,11 @@
 
 'use strict';
 
-var pfx = '___';
+const pfx = '___';
 const jwtUtil = require('../utils/jwtUtil');
 const db = require('../db');
 const testStartTime = new Date();
-const userName = `${pfx}test@refocus.com`;
+const userName = `${pfx}testUser@refocus.com`;
 
 /**
  * By convention, all the resources we create in our tests are named using
@@ -60,7 +60,34 @@ module.exports = {
   gotArrayWithExpectedLength(arr, len) {
     return Array.isArray(arr) && this.gotExpectedLength(arr, len);
   },
-
+  // create one more user
+  createThirdUser() {
+    return db.Profile.create({
+      name: `${pfx}testProfilethird`,
+    })
+    .then((createdProfile) =>
+      db.User.create({
+        profileId: createdProfile.id,
+        name: userName+'third',
+        email: userName+'third',
+        password: 'user123password',
+      })
+    );
+  },
+  // create another user
+  createSecondUser() {
+    return db.Profile.create({
+      name: `${pfx}testProfilesecond`,
+    })
+    .then((createdProfile) =>
+      db.User.create({
+        profileId: createdProfile.id,
+        name: userName+'second',
+        email: userName+'second',
+        password: 'user123password',
+      })
+    );
+  },
   // create user and corresponding token to be used in api tests.
   createToken() {
     return db.Profile.create({
