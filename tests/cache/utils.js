f0d0f8ba8feef2f450dd9c9bdd9292d120c4e422
Ian Goldstein <igoldstein@salesforce.com>
2017-02-27 15:20:15 -0800

    no message

--- tests/cache/utils.js
+++ tests/cache/utils.js
@@ -16,11 +16,9 @@ const tu = require('../testUtils');
 const testStartTime = new Date();
 
 module.exports = {
-  forceDelete: (done) => tu.forceDelete(tu.db.Sample, testStartTime)
+  forceDelete: () => tu.forceDelete(tu.db.Sample, testStartTime)
     .then(() => tu.forceDelete(tu.db.Aspect, testStartTime))
     .then(() => tu.forceDelete(tu.db.Subject, testStartTime))
     .then(() => tu.forceDelete(tu.db.Profile, testStartTime))
-    .then(() => tu.forceDelete(tu.db.User, testStartTime))
-    .then(() => done())
-    .catch(done),
+    .then(() => tu.forceDelete(tu.db.User, testStartTime)),
 };
