6cd0062d0038b6c962d50d33cdbd50e94d40c5b8
Ian Goldstein <igoldstein@salesforce.com>
2017-03-01 14:08:28 -0800

    no message

--- tests/clock/persistSampleStoreJob.js
+++ tests/clock/persistSampleStoreJob.js
@@ -11,9 +11,29 @@
  */
 const expect = require('chai').expect;
 const j = require('../../clock/scheduledJobs/persistSampleStoreJob');
+const tu = require('../testUtils');
+const featureToggles = require('feature-toggles');
+const sampleStore = require('../../cache/sampleStore');
+const initialFeatureState = featureToggles
+  .isFeatureEnabled(sampleStore.constants.featureName);
 
 describe('persistSampleStoreJob', () => {
-  it('ok', (done) => {
+  before(() => tu.toggleOverride(sampleStore.constants.featureName, false));
+  after(() => tu.toggleOverride(sampleStore.constants.featureName,
+    initialFeatureState));
+  it('ok, feature not enabled', (done) => {
+    j.execute()
+    .then((res) => expect(res).to.be.false)
+    .then(() => done())
+    .catch(done);
+  });
+});
+
+describe('persistSampleStoreJob', () => {
+  before(() => tu.toggleOverride(sampleStore.constants.featureName, true));
+  after(() => tu.toggleOverride(sampleStore.constants.featureName,
+    initialFeatureState));
+  it('ok, feature enabled', (done) => {
     j.execute()
     .then((res) => expect(res).to.be.true)
     .then(() => done())
