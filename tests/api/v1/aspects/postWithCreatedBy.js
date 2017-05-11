16bffdd1bc52b3cf345880b3e733de704859b442
Anny He <anny.he@salesforce.com>
2017-05-11 09:42:46 -0700

    add revoked and invalid token tests

--- tests/api/v1/aspects/postWithCreatedBy.js
+++ tests/api/v1/aspects/postWithCreatedBy.js
@@ -12,6 +12,8 @@
 'use strict'; // eslint-disable-line strict
 
 const supertest = require('supertest');
+const adminUser = require('../../../../config').db.adminUser;
+const jwtUtil = require('../../../../utils/jwtUtil');
 const api = supertest(require('../../../../index').app);
 const constants = require('../../../../api/v1/constants');
 const tu = require('../../../testUtils');
@@ -21,9 +23,13 @@ const Aspect = tu.db.Aspect;
 const expect = require('chai').expect;
 const ZERO = 0;
 const ONE = 1;
+const tokenPath = '/v1/tokens';
 
-describe(`api: POST with createdBy ${path}`, () => {
+describe(`api: POST with createdBy when token is NOT enforced ${path}`, () => {
   let token;
+  const predefinedAdminUserToken = jwtUtil.createToken(
+    adminUser.name, adminUser.name
+  );
   before((done) => {
     tu.toggleOverride('returnCreatedBy', true);
     tu.createToken()
@@ -68,6 +74,60 @@ describe(`api: POST with createdBy ${path}`, () => {
       expect(res.body.createdBy).to.be.undefined;
       expect(res.body.user).to.be.undefined;
       done();
+    });
+  });
+
+  it('on invalid token, createdBy and user fields are NOT' +
+    ' returned', (done) => {
+    api.post(path)
+    .set('Authorization', 'iDoNotExist')
+    .send(u.toCreate)
+    .expect(constants.httpStatus.CREATED)
+    .end((err, res ) => {
+      if (err) {
+        done(err);
+      }
+
+      expect(res.body.createdBy).to.be.undefined;
+      expect(res.body.user).to.be.undefined;
+      done();
+    });
+  });
+
+  it('on revoked token, createdBy and user fields are returned', (done) => {
+    api.post(tokenPath)
+    .set('Authorization', token)
+    .send({ name: 'newToken' })
+    .end((err, res) => {
+      if (err) {
+        return done(err);
+      }
+
+      const newToken = res.body.token;
+      return api.post(`${tokenPath}/${res.body.id}/revoke`)
+      .set('Authorization', predefinedAdminUserToken)
+      .send({ })
+      .end((err2, res2) => {
+        if (err2 || res2.body.errors) {
+          return done(err2);
+        }
+
+        api.post(path)
+        .set('Authorization', newToken)
+        .send(u.toCreate)
+        .expect(constants.httpStatus.CREATED)
+        .end((err, res ) => {
+          if (err) {
+            done(err);
+          }
+
+          expect(res.body.createdBy).to.be.an('string');
+          expect(res.body.user).to.be.an('object');
+          expect(res.body.user.name).to.be.an('string');
+          expect(res.body.user.email).to.be.an('string');
+          done();
+        });
+      });
     });
   });
 });
--- tests/api/v1/lenses/postWithCreatedBy.js
+++ tests/api/v1/lenses/postWithCreatedBy.js
@@ -12,6 +12,8 @@
 'use strict'; // eslint-disable-line strict
 
 const featureToggles = require('feature-toggles');
+const adminUser = require('../../../../config').db.adminUser;
+const jwtUtil = require('../../../../utils/jwtUtil');
 const supertest = require('supertest');
 const api = supertest(require('../../../../index').app);
 const constants = require('../../../../api/v1/constants');
@@ -20,9 +22,13 @@ const u = require('./utils');
 const path = '/v1/lenses';
 const expect = require('chai').expect;
 const ZERO = 0;
+const tokenPath = '/v1/tokens';
 
-describe(`api: POST with createdBy ${path}`, () => {
+describe(`api: POST with createdBy when token is NOT enforced ${path}`, () => {
   let token;
+  const predefinedAdminUserToken = jwtUtil.createToken(
+    adminUser.name, adminUser.name
+  );
 
   before((done) => {
     tu.toggleOverride('returnCreatedBy', true);
@@ -73,6 +79,64 @@ describe(`api: POST with createdBy ${path}`, () => {
       expect(res.body.createdBy).to.be.undefined;
       expect(res.body.user).to.be.undefined;
       done();
+    });
+  });
+
+  it('on invalid token, createdBy and user fields are NOT' +
+    ' returned', (done) => {
+    api.post(path)
+    .set('Authorization', 'iDontExist')
+    .field('name', 'testLens')
+    .field('description', 'test description')
+    .attach('library', 'tests/api/v1/apiTestsUtils/lens.zip')
+    .expect(constants.httpStatus.CREATED)
+    .end((err, res ) => {
+      if (err) {
+        done(err);
+      }
+
+      expect(res.body.createdBy).to.be.undefined;
+      expect(res.body.user).to.be.undefined;
+      done();
+    });
+  });
+
+  it('on revoked token, createdBy and user fields are returned', (done) => {
+    api.post(tokenPath)
+    .set('Authorization', token)
+    .send({ name: 'newToken' })
+    .end((err, res) => {
+      if (err) {
+        return done(err);
+      }
+
+      const newToken = res.body.token;
+      return api.post(`${tokenPath}/${res.body.id}/revoke`)
+      .set('Authorization', predefinedAdminUserToken)
+      .send({ })
+      .end((err2, res2) => {
+        if (err2 || res2.body.errors) {
+          return done(err2);
+        }
+
+        api.post(path)
+        .set('Authorization', newToken)
+        .field('name', 'testLens')
+        .field('description', 'test description')
+        .attach('library', 'tests/api/v1/apiTestsUtils/lens.zip')
+        .expect(constants.httpStatus.CREATED)
+        .end((err, res ) => {
+          if (err) {
+            done(err);
+          }
+
+          expect(res.body.createdBy).to.be.an('string');
+          expect(res.body.user).to.be.an('object');
+          expect(res.body.user.name).to.be.an('string');
+          expect(res.body.user.email).to.be.an('string');
+          done();
+        });
+      });
     });
   });
 });
