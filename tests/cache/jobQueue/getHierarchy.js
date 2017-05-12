6b33620a0c83003931f6c0614f6b033500f0a5ea
jgraff <jgraff@salesforce.com>
2017-05-12 13:05:46 -0700

    get hierarchy worker - tests

---
+++ tests/cache/jobQueue/getHierarchy.js
@@ -0,0 +1,340 @@
+/**
+ * Copyright (c) 2017, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
+
+/**
+ * tests/cache/jobQueue/getHierarchy.js
+ */
+'use strict';
+const jobQueue = require('../../../jobQueue/setup').jobQueue;
+const jobType = require('../../../jobQueue/setup').jobType;
+const expect = require('chai').expect;
+const supertest = require('supertest');
+const api = supertest(require('../../../index').app);
+const samstoinit = require('../../../cache/sampleStoreInit');
+const tu = require('../../testUtils');
+const rtu = require('../models/redisTestUtil');
+const constants = require('../../../api/v1/constants');
+const Subject = tu.db.Subject;
+const path = '/v1/subjects/{key}/hierarchy';
+const logger = require('../../../utils/activityLog').logger;
+
+describe(`api: GET using worker process ${path}`, () => {
+
+  before(() => {
+    tu.toggleOverride('enableWorkerProcess', true);
+    tu.toggleOverride('enqueueHierarchy', true);
+    require('../../../worker/jobProcessor');
+  });
+
+  //run normal getHierarchy tests with worker enabled
+  require('../models/subjects/getHierarchy');
+  require('../models/subjects/getHierarchyAspectAndTagsFilters');
+  require('../models/subjects/getHierarchyStatusAndCombinedFilters');
+
+  describe(`api: GET using worker process ${path}`, () => {
+    let token;
+
+    const par = { name: `${tu.namePrefix}NorthAmerica`, isPublished: true };
+    const chi = { name: `${tu.namePrefix}Canada`, isPublished: true };
+    const grn = { name: `${tu.namePrefix}Quebec`, isPublished: true };
+
+    const aspect = {
+      name: 'temperature',
+      timeout: '30s',
+      isPublished: true,
+      rank: 10,
+    };
+
+    const sample1 = { value: '10' };
+
+    let ipar = 0;
+    let ichi = 0;
+    let igrn = 0;
+
+    const invalidKey = '00000';
+    const invalidFilterParams = '?status=aaa,-aaa';
+
+    let nonWorkerResponse;
+
+    // enable sample store
+    before((done) => {
+      tu.toggleOverride('enableRedisSampleStore', true);
+      done();
+    });
+
+    // setup hierarchy
+    before((done) => {
+      Subject.create(par)
+      .then((subj) => {
+        ipar = subj.id;
+      })
+      .then(() => {
+        chi.parentId = ipar;
+        return Subject.create(chi);
+      })
+      .then((subj) => {
+        ichi = subj.id;
+        grn.parentId = ichi;
+        return Subject.create(grn);
+      })
+      .then((sub) => {
+        sample1.subjectId = sub.id;
+        igrn = sub.id;
+        return tu.db.Aspect.create(aspect);
+      })
+      .then((a) => {
+        sample1.aspectId = a.id;
+        return tu.db.Sample.create(sample1);
+      })
+      .then((samp) => {
+        sample1.id = samp.id;
+        return samstoinit.populate();
+      })
+      .then(() => done())
+      .catch(done);
+    });
+
+    // create token
+    before((done) => {
+      tu.createToken()
+      .then((returnedToken) => {
+        token = returnedToken;
+        done();
+      })
+      .catch((err) => done(err));
+    });
+
+    // get non-worker response
+    before((done) => {
+      tu.toggleOverride('enableWorkerProcess', false);
+      tu.toggleOverride('enqueueHierarchy', false);
+      api.get(path.replace('{key}', ipar))
+      .set('Authorization', token)
+      .end((err, res) => {
+        if (err) {
+          done(err);
+        }
+
+        nonWorkerResponse = res.body;
+        done();
+      });
+    });
+
+    // force the job queue to enter the test mode.
+    beforeEach((done) => {
+      jobQueue.testMode.enter(true);
+      done();
+    });
+
+    afterEach(() => {
+      jobQueue.testMode.clear();
+    });
+
+    after(rtu.forceDelete);
+    after(rtu.flushRedis);
+    after(() => tu.toggleOverride('enableRedisSampleStore', false));
+
+    function requestAndExpectNJobs(n, done) {
+      api.get(path.replace('{key}', ipar))
+      .set('Authorization', token)
+      .expect(constants.httpStatus.OK)
+      .end((err) => {
+        if (err) {
+          done(err);
+        }
+
+        expect(jobQueue.testMode.jobs.length).to.equal(n);
+        done();
+      });
+    }
+
+    it('request should only be enqueued if both toggles are set', (done) => {
+      tu.toggleOverride('enableWorkerProcess', false);
+      tu.toggleOverride('enqueueHierarchy', false);
+      requestAndExpectNJobs(0, done);
+    });
+
+    it('request should only be enqueued if both toggles are set', (done) => {
+      tu.toggleOverride('enableWorkerProcess', true);
+      tu.toggleOverride('enqueueHierarchy', false);
+      requestAndExpectNJobs(0, done);
+    });
+
+    it('request should only be enqueued if both toggles are set', (done) => {
+      tu.toggleOverride('enableWorkerProcess', false);
+      tu.toggleOverride('enqueueHierarchy', true);
+      requestAndExpectNJobs(0, done);
+    });
+
+    it('request should only be enqueued if both toggles are set', (done) => {
+      tu.toggleOverride('enableWorkerProcess', true);
+      tu.toggleOverride('enqueueHierarchy', true);
+      requestAndExpectNJobs(1, done);
+    });
+
+    it('examine enqueued data', (done) => {
+      api.get(path.replace('{key}', ipar))
+      .set('Authorization', token)
+      .expect(constants.httpStatus.OK)
+      .end((err) => {
+        if (err) {
+          done(err);
+        }
+
+        // make sure the job is enqueued
+        expect(jobQueue.testMode.jobs.length).to.equal(1);
+
+        // make sure the job type is correct
+        expect(jobQueue.testMode.jobs[0].type).to.equal(jobType.GET_HIERARCHY);
+
+        // make sure the queue has the right data inside it
+        expect(jobQueue.testMode.jobs[0].data.params.key.value).to.equal(ipar);
+
+        done();
+      });
+    });
+
+    it('worker response should be the same as non-worker', (done) => {
+      api.get(path.replace('{key}', ipar))
+      .set('Authorization', token)
+      .expect(constants.httpStatus.OK)
+      .end((err, res) => {
+        if (err) {
+          done(err);
+        }
+
+        expect(res.body).to.deep.equal(nonWorkerResponse);
+        done();
+      });
+    });
+
+    it('test logging', (done) => {
+      tu.toggleOverride('enableApiActivityLogs', true);
+      tu.toggleOverride('enableWorkerActivityLogs', true);
+      let workerLogged = false;
+      let apiLogged = false;
+
+      api.get(path.replace('{key}', ipar))
+      .set('Authorization', token)
+      .expect(constants.httpStatus.OK)
+      .end((err, res) => {
+        if (err) {
+          done(err);
+        }
+
+        expect(workerLogged).to.be.true;
+        expect(apiLogged).to.be.true;
+        tu.toggleOverride('enableApiActivityLogs', false);
+        tu.toggleOverride('enableWorkerActivityLogs', false);
+        done();
+      });
+
+      logger.on('logging', function (transport, level, msg, meta) {
+        const logObj = {};
+        msg.split(' ').forEach((entry) => {
+          logObj[entry.split('=')[0]] = entry.split('=')[1];
+        });
+
+        if (logObj.activity === 'worker') {
+          try {
+            expect(logObj.totalTime).to.match(/\d+ms/);
+            expect(logObj.queueTime).to.match(/\d+ms/);
+            expect(logObj.workTime).to.match(/\d+ms/);
+            expect(logObj.dbTime).to.match(/\d+ms/);
+            expect(logObj.recordCount).to.equal('1');
+            expect(logObj.errorCount).to.equal('0');
+
+            const totalTime = parseInt(logObj.totalTime);
+            const queueTime = parseInt(logObj.queueTime);
+            const workTime = parseInt(logObj.workTime);
+            const dbTime = parseInt(logObj.dbTime);
+
+            expect(workTime).to.be.at.least(dbTime);
+            expect(totalTime).to.be.at.least(workTime);
+            expect(totalTime).to.be.at.least(queueTime);
+            expect(queueTime + workTime).to.be.at.most(totalTime);
+
+            workerLogged = true;
+          } catch (err) {
+            done(err);
+          }
+        }
+
+        if (logObj.activity === 'api') {
+          try {
+            expect(logObj.totalTime).to.match(/\d+ms/);
+            expect(logObj.dbTime).to.match(/\d+ms/);
+            expect(logObj.recordCount).to.equal('1');
+            expect(logObj.responseBytes).to.match(/\d+/);
+
+            const totalTime = parseInt(logObj.totalTime);
+            const dbTime = parseInt(logObj.dbTime);
+
+            expect(totalTime).to.be.above(dbTime);
+
+            apiLogged = true;
+          } catch (err) {
+            done(err);
+          }
+        }
+
+      });
+    });
+
+    it('Error handling - Not Found', (done) => {
+      api.get(path.replace('{key}', invalidKey))
+      .set('Authorization', token)
+      .expect(constants.httpStatus.NOT_FOUND)
+      .end((err, res) => {
+        if (err) {
+          done(err);
+        }
+
+        expect(res.body.errors).to.exist;
+        expect(res.body.errors[0]).to.exist;
+        err = res.body.errors[0];
+        expect(err.message).to.equal('An unexpected ResourceNotFoundError occurred.');
+        expect(err.source).to.equal('Subject');
+        expect(err.value).to.equal(invalidKey);
+        expect(err.type).to.equal('ResourceNotFoundError');
+        expect(err.description).to.equal('');
+        done();
+      });
+    });
+
+    it('Error handling - Bad Request', (done) => {
+      api.get(path.replace('{key}', ipar) + invalidFilterParams)
+      .set('Authorization', token)
+      .expect(constants.httpStatus.BAD_REQUEST)
+      .end((err, res) => {
+        if (err) {
+          done(err);
+        }
+
+        expect(res.body.errors).to.exist;
+        expect(res.body.errors[0]).to.exist;
+        err = res.body.errors[0];
+        expect(err.message).to.equal('Filter should be passed in query ' +
+          'parameter as an include filter or an exclude filter, but not ' +
+          'the combination of both.');
+        expect(err.source).to.equal('');
+        expect(err.value).to.equal('');
+        expect(err.type).to.equal('InvalidFilterParameterError');
+        expect(err.description).to.equal('');
+        done();
+      });
+    });
+
+  });
+
+  after(() => {
+    tu.toggleOverride('enableWorkerProcess', false);
+    tu.toggleOverride('enqueueHierarchy', false);
+  });
+
+});
