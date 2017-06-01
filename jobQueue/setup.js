7dfc30435ae5b4c30d2cd5dcbf995f99f9ed18b9
jgraff <jgraff@salesforce.com>
2017-06-01 14:04:22 -0700

    get hierarchy worker - concurrency config

--- jobQueue/setup.js
+++ jobQueue/setup.js
@@ -40,7 +40,7 @@ if (featureToggles.isFeatureEnabled('instrumentKue')) {
 module.exports = {
   jobConcurrency: {
     BULKUPSERTSAMPLES: conf.bulkUpsertSampleJobConcurrency,
-    GET_HIERARCHY: 1,
+    GET_HIERARCHY: conf.getHierarchyJobConcurrency,
     PERSIST_SAMPLE_STORE: 1,
     SAMPLE_TIMEOUT: 1,
   },
