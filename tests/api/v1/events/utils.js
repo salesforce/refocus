809a461a22eb296742cfd28f9ef9d61e7a10d195
Tausif Muzaffar <tausif.muzaffar@salesforce.com>
2017-07-12 16:38:38 -0400

    test

--- tests/api/v1/events/utils.js
+++ tests/api/v1/events/utils.js
@@ -12,48 +12,19 @@
 'use strict';
 
 const tu = require('../../../testUtils');
-const b = require('../../../db/model/bot/utils');
-const r = require('../../../db/model/room/utils');
 
 const testStartTime = new Date();
 const logLine = 'Sample Event';
-const logLine2 = 'Sample Event 2';
-const logLine3 = 'Sample Event 3';
-
-const bot = b.createStandard();
-const room = r.createStandard();
 
 const standard = {
   log: logLine,
   context: {
     Sample: 'DATA',
   },
-  botId: bot.id,
-  roomId: room.id,
-};
-
-const standard3 = {
-  log: logLine3,
-  context: {
-    Sample: 'DATA',
-  },
-  botId: bot.id,
-};
-
-const standard2 = {
-  log: logLine2,
-  context: {
-    Sample: 'DATA',
-  },
-  roomId: room.id,
 };
 
 module.exports = {
   log: logLine,
-
-  log2: logLine2,
-
-  log3: logLine3,
 
   getStandard() {
     return JSON.parse(JSON.stringify(standard));
@@ -61,14 +32,6 @@ module.exports = {
 
   createStandard() {
     return tu.db.Event.create(standard);
-  },
-
-  createStandard2() {
-    return tu.db.Event.create(standard2);
-  },
-
-  createStandard3() {
-    return tu.db.Event.create(standard3);
   },
 
   forceDelete(done) {
