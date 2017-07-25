066a4d89e0a5abe674f5b7c767c41c1b8a4a33ed
Shriram <sshankar@salesforce.com>
2017-07-25 13:13:07 -0700

    POST generators endpoint working

---
+++ api/v1/helpers/nouns/generators.js
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2016, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
+
+/**
+ * api/v1/helpers/nouns/generators.js
+ */
+'use strict';
+
+const Generator = require('../../../../db/index').Generator;
+
+const m = 'generator';
+
+module.exports = {
+  apiLinks: {
+    DELETE: `Delete this ${m}`,
+    GET: `Retrieve this ${m}`,
+    PATCH: `Update selected attributes of this ${m}`,
+    POST: `Create a new ${m}`,
+    PUT: `Overwrite all attributes of this ${m}`,
+  },
+  baseUrl: '/v1/generators',
+  model: Generator,
+  modelName: 'Generator',
+
+  // define the associations that are to be deleted here
+  belongsToManyAssoc: {
+    users: 'writers',
+  },
+}; // exports
