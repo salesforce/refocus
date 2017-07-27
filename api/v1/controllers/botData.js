7fac1116a6affd01e2460a9e78879adac9a346c3
Tausif Muzaffar <tausif.muzaffar@salesforce.com>
2017-07-27 16:34:52 -0400

    Fix Style

--- api/v1/controllers/botData.js
+++ api/v1/controllers/botData.js
@@ -12,79 +12,9 @@
 'use strict';
 
 const helper = require('../helpers/nouns/botData');
-const doDelete = require('../helpers/verbs/doDelete');
-const doFind = require('../helpers/verbs/doFind');
-const doGet = require('../helpers/verbs/doGet');
 const doPatch = require('../helpers/verbs/doPatch');
-const doPost = require('../helpers/verbs/doPost');
-const doPut = require('../helpers/verbs/doPut');
 
 module.exports = {
-
-  /**
-   * DELETE /botData/{key}
-   *
-   * Deletes the botData and sends it back in the response.
-   *
-   * @param {IncomingMessage} req - The request object
-   * @param {ServerResponse} res - The response object
-   * @param {Function} next - The next middleware function in the stack
-   */
-  deleteBotData(req, res, next) {
-    doDelete(req, res, next, helper);
-  },
-
-  /**
-   * GET /room/{roomID}/bot/{botID}/data
-   *
-   * Finds zero or more botData and sends them back in the response.
-   *
-   * @param {IncomingMessage} req - The request object
-   * @param {ServerResponse} res - The response object
-   * @param {Function} next - The next middleware function in the stack
-   */
-  findRoomBotBotData(req, res, next) {
-    doFind(req, res, next, helper);
-  },
-
-   /**
-   * GET /room/{roomID}/data
-   *
-   * Finds zero or more botData and sends them back in the response.
-   *
-   * @param {IncomingMessage} req - The request object
-   * @param {ServerResponse} res - The response object
-   * @param {Function} next - The next middleware function in the stack
-   */
-  findRoomBotData(req, res, next) {
-    doFind(req, res, next, helper);
-  },
-
-   /**
-   * GET /botData/
-   *
-   * Finds zero or more botData and sends them back in the response.
-   *
-   * @param {IncomingMessage} req - The request object
-   * @param {ServerResponse} res - The response object
-   * @param {Function} next - The next middleware function in the stack
-   */
-  findBotData(req, res, next) {
-    doFind(req, res, next, helper);
-  },
-
-  /**
-   * GET /botData/{key}
-   *
-   * Retrieves the botData and sends it back in the response.
-   *
-   * @param {IncomingMessage} req - The request object
-   * @param {ServerResponse} res - The response object
-   * @param {Function} next - The next middleware function in the stack
-   */
-  getBotData(req, res, next) {
-    doGet(req, res, next, helper);
-  },
 
   /**
    * PATCH /botData/{key}
@@ -97,19 +27,6 @@ module.exports = {
    */
   patchBotData(req, res, next) {
     doPatch(req, res, next, helper);
-  },
-
-  /**
-   * POST /botData
-   *
-   * Creates a new botData and sends it back in the response.
-   *
-   * @param {IncomingMessage} req - The request object
-   * @param {ServerResponse} res - The response object
-   * @param {Function} next - The next middleware function in the stack
-   */
-  postBotData(req, res, next) {
-    doPost(req, res, next, helper);
   },
 
 }; // exports
