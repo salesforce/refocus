929ab80e30df58d83a080168b486adb8ef1a8a2c
Kris Foster <kfoster@salesforce.com>
2017-07-28 11:13:16 +0100

    general profile GET tests added

---
+++ tests/api/v1/profiles/get.js
@@ -0,0 +1,113 @@
+/**
+ * Copyright (c) 2017, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
+
+/**
+ * tests/api/v1/profiles/get.js
+ */
+'use strict';
+
+const supertest = require('supertest');
+const api = supertest(require('../../../../index').app);
+const constants = require('../../../../api/v1/constants');
+const tu = require('../../../testUtils');
+const u = require('./utils');
+const path = '/v1/profiles';
+const expect = require('chai').expect;
+const Profile = tu.db.Profile;
+const adminProfile = require('../../../../config').db.adminProfile;
+
+describe(`api: GET ${path}`, () => {
+  const pname = `${tu.namePrefix}1`;
+  let token;
+
+  before((done) => {
+    tu.createToken()
+    .then((returnedToken) => {
+      token = returnedToken;
+      done();
+    })
+    .catch(done);
+  });
+
+  before((done) => {
+    Profile.create({
+      name: pname,
+    })
+    .then(() => done())
+    .catch(done);
+  });
+
+  after(u.forceDelete);
+
+  it('standard test profile found', (done) => {
+    api.get(`${path}/${pname}`)
+    .set('Authorization', token)
+    .expect(constants.httpStatus.OK)
+    .end((err, res) => {
+      if (err) {
+        done(err);
+      }
+      expect(res.body).to.have.property('name', pname);
+      expect(res.body).to.have.property('aspectAccess', 'r');
+      expect(res.body).to.have.property('botAccess', 'r');
+      expect(res.body).to.have.property('eventAccess', 'r');
+      expect(res.body).to.have.property('lensAccess', 'r');
+      expect(res.body).to.have.property('perspectiveAccess', 'r');
+      expect(res.body).to.have.property('profileAccess', 'r'); 
+      expect(res.body).to.have.property('roomAccess', 'rw');
+      expect(res.body).to.have.property('roomTypeAccess', 'r');  
+      expect(res.body).to.have.property('sampleAccess', 'r'); 
+      expect(res.body).to.have.property('subjectAccess', 'r');
+      expect(res.body).to.have.property('userAccess', 'r');
+      done();
+    });
+  });
+
+  it('Admin profile found', (done) => {
+    api.get(`${path}/${adminProfile.name}`)
+    .set('Authorization', token)
+    .expect(constants.httpStatus.OK)
+    .end((err, res) => {
+      if (err) {
+        done(err);
+      }
+      expect(res.body).to.have.property('name', adminProfile.name);
+      expect(res.body).to.have.property('aspectAccess', 'rw');
+      expect(res.body).to.have.property('botAccess', 'rw');
+      expect(res.body).to.have.property('eventAccess', 'rw');
+      expect(res.body).to.have.property('lensAccess', 'rw');
+      expect(res.body).to.have.property('perspectiveAccess', 'rw');
+      expect(res.body).to.have.property('profileAccess', 'rw'); 
+      expect(res.body).to.have.property('roomAccess', 'rw');
+      expect(res.body).to.have.property('roomTypeAccess', 'rw');  
+      expect(res.body).to.have.property('sampleAccess', 'rw'); 
+      expect(res.body).to.have.property('subjectAccess', 'rw');
+      expect(res.body).to.have.property('userAccess', 'rw');
+      done();
+    });
+  });
+
+  it('profiles array returned', (done) => {
+    api.get(`${path}`)
+    .expect(constants.httpStatus.OK)
+    .end((err, res) => {
+      if (err) {
+        done(err);
+      }
+      expect(res.body).to.be.instanceof(Array);
+      done();
+    });
+  });
+
+  it('Profile not found', (done) => {
+    api.get(`${path}/who@what.com`)
+    .set('Authorization', token)
+    .expect(constants.httpStatus.NOT_FOUND)
+    .end(() => done());
+  });
+});
