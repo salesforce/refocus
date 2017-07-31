9f2517b6a8d193e3655f392a584137ade5f0e883
Tausif Muzaffar <tausif.muzaffar@salesforce.com>
2017-07-31 19:45:12 -0400

    GET Bot Data api (#452)

--- api/v1/controllers/botData.js
+++ api/v1/controllers/botData.js
@@ -12,6 +12,8 @@
 'use strict';
 
 const helper = require('../helpers/nouns/botData');
+const doFind = require('../helpers/verbs/doFind');
+const doGet = require('../helpers/verbs/doGet');
 const doPatch = require('../helpers/verbs/doPatch');
 const doDelete = require('../helpers/verbs/doDelete');
 
@@ -41,6 +43,32 @@ module.exports = {
    */
   patchBotData(req, res, next) {
     doPatch(req, res, next, helper);
+  },
+
+  /**
+   * GET /botData/, /room/{roomID}/bot/{botID}/data, /room/{roomID}/data
+   *
+   * Finds zero or more botData and sends them back in the response.
+   *
+   * @param {IncomingMessage} req - The request object
+   * @param {ServerResponse} res - The response object
+   * @param {Function} next - The next middleware function in the stack
+   */
+  findBotData(req, res, next) {
+    doFind(req, res, next, helper);
+  },
+
+  /**
+   * GET /botData/{key}
+   *
+   * Retrieves the botData and sends it back in the response.
+   *
+   * @param {IncomingMessage} req - The request object
+   * @param {ServerResponse} res - The response object
+   * @param {Function} next - The next middleware function in the stack
+   */
+  getBotData(req, res, next) {
+    doGet(req, res, next, helper);
   },
 
 }; // exports
