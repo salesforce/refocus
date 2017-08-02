1702ffe85a5b80a642c9280e352bd8b39b9d9d3e
Kris Foster <kfoster@salesforce.com>
2017-08-02 16:31:47 +0100

    migration to fix profile model

---
+++ migrations/20170802152927-add_imc_profile_migration.js
@@ -0,0 +1,27 @@
+module.exports = {
+  up: function (qi, Sequelize) {
+    return qi.addColumn('Profiles', 'botAccess', {
+      type: Sequelize.ENUM('r', 'rw'),
+      defaultValue: 'r',
+    })
+    .then(() => qi.addColumn('Profiles', 'eventAccess', {
+      type: Sequelize.ENUM('r', 'rw'),
+      defaultValue: 'r',
+    }))
+    .then(() => qi.addColumn('Profiles', 'roomAccess', {
+      type: Sequelize.ENUM('r', 'rw'),
+      defaultValue: 'r',
+    }))
+    .then(() => qi.addColumn('Profiles', 'roomTypeAccess', {
+      type: Sequelize.ENUM('r', 'rw'),
+      defaultValue: 'r',
+    }));
+  },
+
+  down: function (qi, Sequelize) {
+    return qi.removeColumn('Profiles', 'botAccess')
+    .then(() => qi.removeColumn('Profiles', 'eventAccess'))
+    .then(() => qi.removeColumn('Profiles', 'roomAccess'))
+    .then(() => qi.removeColumn('Profiles', 'roomTypeAccess'));
+  }
+};
