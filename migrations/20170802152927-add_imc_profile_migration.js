26c3a5d8e7999b896248df98eec38b1eaf33ea71
Tausif Muzaffar <tausif.muzaffar@salesforce.com>
2017-08-10 13:35:27 -0500

    add check for if property already exists

--- migrations/20170802152927-add_imc_profile_migration.js
+++ migrations/20170802152927-add_imc_profile_migration.js
@@ -1,27 +1,71 @@
 module.exports = {
   up: function (qi, Sequelize) {
-    return qi.addColumn('Profiles', 'botAccess', {
-      type: Sequelize.ENUM('r', 'rw'),
-      defaultValue: 'r',
+    qi.sequelize.query('SELECT * FROM "Profiles" LIMIT 1',
+      { type: qi.sequelize.QueryTypes.SELECT })
+    .then((profiles) => {
+      const profile = profiles[0];
+      if (!profile.hasOwnProperty('botAccess')) {
+        qi.addColumn('Profiles', 'botAccess', {
+          type: Sequelize.ENUM('r', 'rw'),
+          defaultValue: 'r',
+        });
+      }
+      return profile;
     })
-    .then(() => qi.addColumn('Profiles', 'eventAccess', {
-      type: Sequelize.ENUM('r', 'rw'),
-      defaultValue: 'r',
-    }))
-    .then(() => qi.addColumn('Profiles', 'roomAccess', {
-      type: Sequelize.ENUM('r', 'rw'),
-      defaultValue: 'rw',
-    }))
-    .then(() => qi.addColumn('Profiles', 'roomTypeAccess', {
-      type: Sequelize.ENUM('r', 'rw'),
-      defaultValue: 'r',
-    }));
+    .then((profile) => {
+      if (!profile.hasOwnProperty('eventAccess')) {
+        qi.addColumn('Profiles', 'eventAccess', {
+          type: Sequelize.ENUM('r', 'rw'),
+          defaultValue: 'r',
+        });
+      }
+      return profile;
+    })
+    .then((profile) => {
+      if (!profile.hasOwnProperty('roomAccess')) {
+        qi.addColumn('Profiles', 'roomAccess', {
+          type: Sequelize.ENUM('r', 'rw'),
+          defaultValue: 'rw',
+        });
+      }
+      return profile;
+    })
+    .then((profile) => {
+      if (!profile.hasOwnProperty('roomTypeAccess')) {
+        qi.addColumn('Profiles', 'roomTypeAccess', {
+          type: Sequelize.ENUM('r', 'rw'),
+          defaultValue: 'r',
+        });
+      }
+    });
   },
 
   down: function (qi, Sequelize) {
-    return qi.removeColumn('Profiles', 'botAccess')
-    .then(() => qi.removeColumn('Profiles', 'eventAccess'))
-    .then(() => qi.removeColumn('Profiles', 'roomAccess'))
-    .then(() => qi.removeColumn('Profiles', 'roomTypeAccess'));
+    qi.sequelize.query('SELECT * FROM "Profiles" LIMIT 1',
+      { type: qi.sequelize.QueryTypes.SELECT })
+    .then((profiles) => {
+      const profile = profiles[0];
+      if (profile.hasOwnProperty('botAccess')) {
+        qi.removeColumn('Profiles', 'botAccess');
+      }
+      return profile;
+    })
+    .then((profile) => {
+      if (profile.hasOwnProperty('eventAccess')) {
+        qi.removeColumn('Profiles', 'eventAccess');
+      }
+      return profile;
+    })
+    .then((profile) => {
+      if (profile.hasOwnProperty('roomAccess')) {
+        qi.removeColumn('Profiles', 'roomAccess');
+      }
+      return profile;
+    })
+    .then((profile) => {
+      if (profile.hasOwnProperty('roomTypeAccess')) {
+        qi.removeColumn('Profiles', 'roomTypeAccess');
+      }
+    });
   },
 };
