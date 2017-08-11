c370f3183cb86dfb009019ba286a10eef08c7b4b
Kris Foster <kfoster@salesforce.com>
2017-08-11 14:55:22 +0100

    added null case

--- db/model/roomType.js
+++ db/model/roomType.js
@@ -93,6 +93,9 @@ module.exports = function roomType(seq, dataTypes) {
        * @param {RoomType} inst - The newly-created instance
        */
       afterCreate(inst /* , opts */) {
+        if(inst.dataValues.bots === null){
+          return;
+        }
         inst.dataValues.bots.map((botName) => {
           seq.models.Bot.findOne({ where: { name: botName } })
           .then((o) => {
