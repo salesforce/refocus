351d75d67284385ffdcd34460f2d2855d1f45f9e
Tausif Muzaffar <tausif.muzaffar@salesforce.com>
2017-08-29 10:24:22 -0400

    no message

--- view/rooms/list/ListController.js
+++ view/rooms/list/ListController.js
@@ -24,35 +24,45 @@ class ListController extends React.Component {
   render() {
     const { values } = this.props;
     return (
-      <div className="slds-grid">
-        <table className="slds-table slds-table--bordered slds-table-cell-buffer">
-          <thead>
-            <tr className="slds-text-title--caps">
-              <th scope="col">
-                <div className="slds-truncate" title="roomID">ID</div>
-              </th>
-              <th scope="col">
-                <div className="slds-truncate" title="roomName">Name</div>
-              </th>
-              <th scope="col">
-                <div className="slds-truncate" title="type">Type</div>
-              </th>
-              <th scope="col">
-                <div className="slds-truncate" title="active">Active</div>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {values.map((room) => {
-              return <tr>
-                <td><a href={'/rooms/'+room.id}>{room.id}</a></td>
-                <td><a href={'/rooms/'+room.id}>{room.name}</a></td>
-                <td>{room.type}</td>
-                <td>{room.active ? 'True' : 'False'}</td>
-              </tr>;
-            })}
-          </tbody>
-        </table>
+      <div>
+        <div className="slds-page-header">
+          <div className="slds-media">
+            <div className="slds-media__body">
+              <h1 className="slds-page-header__title slds-truncate slds-align-middle" title="Refocus Rooms">Refocus Rooms</h1>
+              <p className="slds-text-body_small slds-line-height_reset">Number of rooms: {values.length}</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <table className="slds-table slds-table--bordered slds-table-cell-buffer">
+            <thead>
+              <tr className="slds-text-title--caps">
+                <th scope="col">
+                  <div className="slds-truncate" title="roomID">ID</div>
+                </th>
+                <th scope="col">
+                  <div className="slds-truncate" title="roomName">Name</div>
+                </th>
+                <th scope="col">
+                  <div className="slds-truncate" title="type">Type</div>
+                </th>
+                <th scope="col">
+                  <div className="slds-truncate" title="active">Active</div>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {values.map((room) => {
+                return <tr>
+                  <td><a href={'/rooms/'+room.id}>{room.id}</a></td>
+                  <td><a href={'/rooms/'+room.id}>{room.name}</a></td>
+                  <td>{room.type}</td>
+                  <td>{room.active ? 'True' : 'False'}</td>
+                </tr>;
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
