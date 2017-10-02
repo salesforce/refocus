fdd0ba569cf1a4c7c5cad7ce65fc632a6756347d
Anny He <anny.he@salesforce.com>
2017-10-02 09:21:12 -0700

    need npm install in client

--- UItest.sh
+++ UItest.sh
@@ -3,11 +3,10 @@
 # populate the db with data for UI test
 
 # need to send server to the background, to run additional commands
-git clone https://github.com/iamigo/refocus-client-example.git
 node . &
 TASK_PID=$!
 sleep 5 # for the server process
-cd refocus-client-example
+cd ../refocus-client-example
 node subjectsAndAspects.js &
 node perspectiveAndLens.js &
 
