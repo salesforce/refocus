481ae3765ca56eaa795a64a2ea1f2f9f380cd479
Anny He <anny.he@salesforce.com>
2017-10-02 11:15:11 -0700

    set up db

--- setupClient.sh
+++ setupClient.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # sets up client to push data to refocus
-if [ ! -d ../refocus-client-example ]; then
-  cd ..
-  git clone https://github.com/iamigo/refocus-client-example.git
-  cd refocus-client-example
-  npm install
-  cd ../refocus
-fi
+cd ..
+rm -rf refocus-client-example
+git clone https://github.com/iamigo/refocus-client-example.git
+cd refocus-client-example
+npm install
+cd ../refocus
+
 
