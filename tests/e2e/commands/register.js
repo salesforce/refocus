80ecbc6a2d7fd67e6a6df8ef89e78cd6429e148e
Anny He <anny.he@salesforce.com>
2017-10-02 15:00:09 -0700

    initial commit with working e2e tests

--- tests/e2e/commands/register.js
+++ tests/e2e/commands/register.js
@@ -5,11 +5,14 @@
  * For full license text, see LICENSE.txt file in the repo root or
  * https://opensource.org/licenses/BSD-3-Clause
  */
-exports.command = function login(username, password) {
+
+exports.command = function register(username, password) {
   this
     .waitForElementVisible('body', 500)
     .setValue('input[name=username]', username)
+    .setValue('input[name=email]', username + '@a.com')
     .setValue('input[name=password]', password)
+    .setValue('input[name=repassword]', password)
     .click('button[type=submit]')
     .pause(1000)
     .waitForElementVisible('.slds-lookup__search-input', 1000)
