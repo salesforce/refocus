5dff54d77d055c6e0a2aa2e0ce16efcbbb633d7c
Anny He <anny.he@salesforce.com>
2017-10-02 11:50:13 -0700

    consolidate scripts

--- UItest.sh
+++ UItest.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 # populate the db with data for UI test
+cd ..
+if [ ! -d ./refocus-client-example ]; then
+  rm -rf
+  git clone https://github.com/iamigo/refocus-client-example.git
+  cd refocus-client-example
+  npm install
+  cd ../refocus
+else
+  cd refocus
+fi
 
 # need to send server to the background, to run additional commands
 node . &
