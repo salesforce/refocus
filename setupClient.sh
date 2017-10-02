159c0111c694a9a94ed9fda105b5c0943f5f781d
Anny He <anny.he@salesforce.com>
2017-10-02 09:28:36 -0700

    minor tweaks

--- setupClient.sh
+++ setupClient.sh
@@ -2,8 +2,10 @@
 
 # sets up client to push data to refocus
 if [ ! -d ../refocus-client-example ]; then
+  cd ..
   git clone https://github.com/iamigo/refocus-client-example.git
   cd refocus-client-example
-  npm run install
+  npm install
+  cd ../refocus
 fi
 
