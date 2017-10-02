5c1574c8f69211340baba012e2a45de96171f9e5
Anny He <anny.he@salesforce.com>
2017-10-02 14:01:28 -0700

    save directory path as vars

--- scripts/UItest.sh
+++ scripts/UItest.sh
@@ -3,21 +3,34 @@
 # Populate the db with data for UI test
 
 # clones the client to send data to refocus
-cd ..
+refocusDir=$(pwd)
+echo $refocusDir
+cd .. # go one dir above the refocus dir
 if [ ! -d ./refocus-client-example ]; then
   rm -rf
   git clone https://github.com/iamigo/refocus-client-example.git
   cd refocus-client-example
+  refocusClientDir=$(pwd)
   npm install
-  cd ../refocus
 else
-  cd refocus
+  cd refocus-client-example
+  refocusClientDir=$(pwd)
 fi
+echo $refocusClientDir
 
+cd $refocusDir
 # starts and sends server to the background, to run additional commands
 node . &
 TASK_PID=$!
 sleep 5 # for the server process
-. ./loadResources.sh # calls the client code
+
+# run from refocus dir
+cd $refocusClientDir
+echo 'populating subjects, aspects, lens, and perspective'
+node subjectsAndAspects.js
+node perspectiveAndLens.js
+cd $refocusDir
+
 sleep 10 # for the server process
 kill $TASK_PID
+
