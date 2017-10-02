b3a805e355024d30cf4282ed3003e1f4eec23a35
Anny He <anny.he@salesforce.com>
2017-10-02 11:42:16 -0700

    took out source

--- setupClient.sh
+++ setupClient.sh
@@ -2,10 +2,15 @@
 
 # sets up client to push data to refocus
 cd ..
-rm -rf refocus-client-example
-git clone https://github.com/iamigo/refocus-client-example.git
-cd refocus-client-example
-npm install
-cd ../refocus
+if [ ! -d ./refocus-client-example ]; then
+  rm -rf
+  git clone https://github.com/iamigo/refocus-client-example.git
+  cd refocus-client-example
+  npm install
+  cd ../refocus
+else
+  cd refocus
+fi
+
 
 
