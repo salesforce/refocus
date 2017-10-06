123bf97be39aba066f3de1bb3fb445021df63cbc
annyhe <anny.he@salesforce.com>
2017-10-06 09:59:18 -0700

    GET /v1/lenses/:key should have a "user" attribute when returnUser toggle is on (#634)

--- tests/api/v1/lenses/put.js
+++ tests/api/v1/lenses/put.js
@@ -21,88 +21,134 @@ const expect = require('chai').expect;
 describe('tests/api/v1/lenses/put.js >', () => {
   let lensId;
   let token;
+  let userId;
 
   before((done) => {
-    tu.createToken()
-    .then((returnedToken) => {
-      token = returnedToken;
+    tu.createUserAndToken()
+    .then((obj) => {
+      userId = obj.user.id;
+      token = obj.token;
       done();
     })
     .catch(done);
   });
-
-  before((done) => {
-    u.doSetup()
-    .then((lens) => {
-      lensId = lens.id;
-      done();
-    })
-    .catch(done);
-  });
-
-  after(u.forceDelete);
   after(tu.forceDeleteUser);
 
-  it('update name to different case', (done) => {
-    const newName = u.name.toLowerCase();
-    api.put(`${path}/${lensId}`)
-    .set('Authorization', token)
-    .field('name', newName)
-    .attach('library', 'tests/api/v1/apiTestsUtils/lens.zip')
-    .expect(constants.httpStatus.OK)
-    .end((err, res) => {
-      if (err) {
-        return done(err);
-      }
+  describe('with returnUser toggle on, user object should be returned: ', () => {
+    before((done) => {
+      tu.toggleOverride('returnUser', true);
+      const lens = u.getLens({ installedBy: userId });
+      u.doSetup(lens)
+      .then((lens) => {
+        expect(lens.installedBy).to.equal(userId);
+        lensId = lens.id;
+        done();
+      })
+      .catch(done);
+    });
+    after(u.forceDelete);
+    after(() => tu.toggleOverride('returnUser', false));
 
-      expect(res.body.name).to.equal(newName);
-      done();
+    it('update description', (done) => {
+      api.put(`${path}/${lensId}`)
+      .set('Authorization', token)
+      .field('description', 'changed description')
+      .attach('library', 'tests/api/v1/apiTestsUtils/lens.zip')
+      .expect(constants.httpStatus.OK)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        expect(res.body.description).to.equal('changed description');
+        expect(res.body.user).to.be.an('object');
+        expect(res.body.installedBy).to.equal(userId);
+        done();
+      });
     });
   });
 
-  it('update description', (done) => {
-    api.put(`${path}/${lensId}`)
-    .set('Authorization', token)
-    .field('description', 'changed description')
-    .attach('library', 'tests/api/v1/apiTestsUtils/lens.zip')
-    .expect(constants.httpStatus.OK)
-    .end((err, res) => {
-      if (err) {
-        return done(err);
-      }
-
-      expect(res.body.description).to.equal('changed description');
-      done();
+  describe('with returnUser toggle off, user should not be returned', () => {
+    before((done) => {
+      u.doSetup()
+      .then((lens) => {
+        lensId = lens.id;
+        done();
+      })
+      .catch(done);
     });
-  });
 
-  it('isPublished set to default', (done) => {
-    api.put(`${path}/${lensId}`)
-    .set('Authorization', token)
-    .attach('library', 'tests/api/v1/apiTestsUtils/lens.zip')
-    .expect(constants.httpStatus.OK)
-    .end((err, res) => {
-      if (err) {
-        return done(err);
-      }
+    after(u.forceDelete);
 
-      expect(res.body.isPublished).to.not.be.true;
-      done();
+    it('update name to different case', (done) => {
+      const newName = u.name.toLowerCase();
+      api.put(`${path}/${lensId}`)
+      .set('Authorization', token)
+      .field('name', newName)
+      .attach('library', 'tests/api/v1/apiTestsUtils/lens.zip')
+      .expect(constants.httpStatus.OK)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        expect(res.body.user).to.be.undefined;
+        expect(res.body.installedBy).to.be.undefined;
+        expect(res.body.name).to.equal(newName);
+        done();
+      });
     });
-  });
 
-  it('name overwritten by sourceName', (done) => {
-    api.put(`${path}/${lensId}`)
-    .set('Authorization', token)
-    .attach('library', 'tests/api/v1/apiTestsUtils/lens.zip')
-    .expect(constants.httpStatus.OK)
-    .end((err, res) => {
-      if (err) {
-        return done(err);
-      }
+    it('update description', (done) => {
+      api.put(`${path}/${lensId}`)
+      .set('Authorization', token)
+      .field('description', 'changed description')
+      .attach('library', 'tests/api/v1/apiTestsUtils/lens.zip')
+      .expect(constants.httpStatus.OK)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
 
-      expect(res.body.name).to.equal(res.body.sourceName);
-      done();
+        expect(res.body.user).to.be.undefined;
+        expect(res.body.installedBy).to.be.undefined;
+        expect(res.body.description).to.equal('changed description');
+        done();
+      });
+    });
+
+    it('isPublished set to default', (done) => {
+      api.put(`${path}/${lensId}`)
+      .set('Authorization', token)
+      .attach('library', 'tests/api/v1/apiTestsUtils/lens.zip')
+      .expect(constants.httpStatus.OK)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        expect(res.body.user).to.be.undefined;
+        expect(res.body.installedBy).to.be.undefined;
+        expect(res.body.isPublished).to.be.false;
+        done();
+      });
+    });
+
+    it('name overwritten by sourceName', (done) => {
+      api.put(`${path}/${lensId}`)
+      .set('Authorization', token)
+      .attach('library', 'tests/api/v1/apiTestsUtils/lens.zip')
+      .expect(constants.httpStatus.OK)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        expect(res.body.user).to.be.undefined;
+        expect(res.body.installedBy).to.be.undefined;
+        expect(res.body.name).to.equal(res.body.sourceName);
+        done();
+      });
     });
   });
 });
