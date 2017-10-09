3fdf0082d8768d08c629985f5b46bfc457014afc
Pallavi Singh <pallavi9@gmail.com>
2017-10-09 13:03:55 -0700

    POST, PUT and PATCH for aspects: helpEmail or helpUrl required (#629)

--- config/toggles.js
+++ config/toggles.js
@@ -140,6 +140,11 @@ const shortTermToggles = {
   // publish partial sample to the subscribers
   publishPartialSample: environmentVariableTrue(pe, 'PUBLISH_PARTIAL_SAMPLE'),
 
+  // require helpEmail or helpUrl in POST/PUT/PATCH of aspects and subjects
+  requireHelpEmailOrHelpUrl: environmentVariableTrue(
+    pe, 'REQUIRE_HELP_EMAIL_OR_HELP_URL'
+  ),
+
 }; // shortTermToggles
 
 featureToggles.load(Object.assign({}, longTermToggles, shortTermToggles));
