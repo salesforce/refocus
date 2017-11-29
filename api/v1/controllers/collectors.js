860d862fd78454730901baf09acce04ccdfa0fb4
Anny He <anny.he@salesforce.com>
2017-11-29 11:55:03 -0800

    initial commit

--- api/v1/controllers/collectors.js
+++ api/v1/controllers/collectors.js
@@ -302,7 +302,11 @@ function heartbeat(req, res, next) {
   .then(() => Promise.all(
     retval.generatorsAdded.map((sg) =>
       attachTemplate(sg)
-      .then((sg) => reEncryptSGContextValues(sg, authToken, timestamp))
+      .then((sg) => {
+        const userName = sg.generatorTemplate.user.name;
+        sg.token = jwtUtil.createToken(userName, userName);
+        return reEncryptSGContextValues(sg, authToken, timestamp);
+      })
     )
   ))
   .then((added) => retval.generatorsAdded = added)
