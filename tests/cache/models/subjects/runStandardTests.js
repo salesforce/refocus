d657d08f6a0275e9ff724d8cd6e22668ad7f79a7
jgraff <jgraff@salesforce.com>
2018-01-04 19:56:55 -0800

    subject caching - run standard tests with cache enabled 2

---
+++ tests/cache/models/subjects/runStandardTests.js
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2017, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
+
+/**
+ * tests/cache/models/subjects/runStandardTests.js
+ */
+'use strict'; // eslint-disable-line strict
+const tu = require('../../../testUtils');
+const requireDir = require('require-dir');
+
+/*
+This test runs the standard subject api tests with the cache enabled.
+Note that this must be run in a separate command from the api tests,
+otherwise these tests will not run because files can't be required
+twice in the same process.
+*/
+
+describe('tests/cache/models/subjects/runStandardTests.js, ', () => {
+  before(() => tu.toggleOverride('enableRedisSampleStore', true));
+  before(() => tu.toggleOverride('getSubjectFromCache', true));
+  after(() => tu.toggleOverride('enableRedisSampleStore', false));
+  after(() => tu.toggleOverride('getSubjectFromCache', false));
+
+  describe('run standard tests with cache enabled', () => {
+    requireDir('../../../api/v1/subjects');
+  });
+});
