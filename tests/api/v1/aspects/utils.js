0596ff0d5bceffe84610225d5aafc107fbbe2981
Shriram <sshankar@salesforce.com>
2018-03-16 14:23:21 -0700

    changed api/v1/aspect tests

--- tests/api/v1/aspects/utils.js
+++ tests/api/v1/aspects/utils.js
@@ -12,8 +12,6 @@
 'use strict';
 const tu = require('../../../testUtils');
 const samstoinit = require('../../../../cache/sampleStoreInit');
-const featureToggles = require('feature-toggles');
-const rcli = require('../../../../cache/redisCache').client.sampleStore;
 const Promise = require('bluebird');
 
 const testStartTime = new Date();
@@ -29,14 +27,6 @@ const subjectToCreate = {
   name: `${tu.namePrefix}TEST_SUBJECT`,
 };
 
-function flushRedisIfEnabled() {
-  if (featureToggles.isFeatureEnabled('enableRedisSampleStore')) {
-    return rcli.flushallAsync();
-  } else {
-    return Promise.resolve();
-  }
-}
-
 module.exports = {
   toCreate: {
     name: `${tu.namePrefix}ASPECTNAME`,
@@ -51,25 +41,17 @@ module.exports = {
 
   forceDelete(done) {
     Promise.join(
-      flushRedisIfEnabled(),
-      tu.forceDelete(tu.db.Sample, testStartTime)
-      .then(() => tu.forceDelete(tu.db.Aspect, testStartTime))
+      samstoinit.eradicate(),
+      tu.forceDelete(tu.db.Aspect, testStartTime)
       .then(() => tu.forceDelete(tu.db.Subject, testStartTime))
     )
     .then(() => done())
     .catch(done);
   },
 
-  populateRedisIfEnabled(done) {
-    if (
-      featureToggles.isFeatureEnabled('enableRedisSampleStore')
-    ) {
-      samstoinit.eradicate()
-      .then(() => samstoinit.populate())
-      .then(() => done())
-      .catch(done);
-    } else {
-      done();
-    }
+  populateRedis(done) {
+    samstoinit.populate()
+    .then(() => done())
+    .catch(done);
   },
 };
