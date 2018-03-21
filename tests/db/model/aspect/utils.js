129682d8aeef3fd30f00dee4e46286d7ed48b1a2
Shriram <sshankar@salesforce.com>
2018-03-21 13:53:52 -0700

    fixed indentation and skipped calculate status test

--- tests/db/model/aspect/utils.js
+++ tests/db/model/aspect/utils.js
@@ -54,10 +54,10 @@ module.exports = {
 
   forceDelete(done) {
     Promise.join(rcli.flushallAsync(),
-    tu.forceDelete(tu.db.Aspect, testStartTime)
-    .then(() => tu.forceDelete(tu.db.Subject, testStartTime))
-    .then(() => tu.forceDelete(tu.db.User, testStartTime))
-    .then(() => tu.forceDelete(tu.db.Profile, testStartTime))
+      tu.forceDelete(tu.db.Aspect, testStartTime)
+      .then(() => tu.forceDelete(tu.db.Subject, testStartTime))
+      .then(() => tu.forceDelete(tu.db.User, testStartTime))
+      .then(() => tu.forceDelete(tu.db.Profile, testStartTime))
     )
     .then(() => done())
     .catch(done);
