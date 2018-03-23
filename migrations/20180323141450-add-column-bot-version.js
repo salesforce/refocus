2267412def3ec6ff039b917039ce06aecac4028c
ksmuga <ksmuga@salesforce.com>
2018-03-23 14:46:45 +0100

    not-null default value for version during migration

--- migrations/20180323141450-add-column-bot-version.js
+++ migrations/20180323141450-add-column-bot-version.js
@@ -4,7 +4,7 @@ module.exports = {
   up(qi, Sequelize) {
     return qi.addColumn('Bots', 'version', {
       type: Sequelize.STRING(10),
-      defaultValue: null,
+      defaultValue: '1.0.0',
     });
   },
 
