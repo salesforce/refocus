21852f46e040b041f6ed6c08ca5b337635b25b0d
Shriram <sshankar@salesforce.com>
2018-04-19 13:59:35 -0700

    added comment to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or
  * https://opensource.org/licenses/BSD-3-Clause
+ *
+ * NOTE: we do not use webpack in production. Use it for front development with
+ * with hot reload
  */
 'use strict';
 const pe = process.env; // eslint-disable-line no-process-env
