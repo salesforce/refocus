d0a5fd8edab2352b2dc65dc8986cdd1043a8d6cd
Ian Goldstein <igoldstein@salesforce.com>
2018-04-30 13:35:21 -0700

    no message

--- tests/api/v1/index/xfwd.js
+++ tests/api/v1/index/xfwd.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2017, salesforce.com, inc.
+ * Copyright (c) 2018, salesforce.com, inc.
  * All rights reserved.
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or
@@ -10,15 +10,10 @@
  * tests/api/v1/index/xfwd.js
  */
 'use strict';
-const toggle = require('feature-toggles');
 const supertest = require('supertest');
 const api = supertest(require('../../../../index').app);
 const constants = require('../../../../api/v1/constants');
-const tu = require('../../../testUtils');
-const path = '/v1';
 const expect = require('chai').expect;
-const XFWD_FEATURE = 'rejectMultipleXForwardedFor';
-const initialState = toggle.isFeatureEnabled(XFWD_FEATURE);
 
 describe('tests/api/v1/index/xfwd.js >', () => {
   describe('default allow multiple x-forwarded-for ip addresses >', () => {
