1d00185314d76f91e9c119233d09b2c844d8d2b1
Ian Goldstein <igoldstein@salesforce.com>
2018-05-03 12:54:34 -0700

    Add collector heartbeat latency tolerance to config.

--- config/collectorConfig.js
+++ config/collectorConfig.js
@@ -14,12 +14,41 @@
 'use strict'; // eslint-disable-line strict
 const pe = process.env; // eslint-disable-line no-process-env
 
+// Don't allow admin to override heartbeat interval or latency less than 1 sec.
+const FLOOR = 1000; // milliseconds
+const INTERVAL_DEFAULT = 15; // seconds
+const LATENCY_DEFAULT = 5; // seconds
+
+let heartbeatIntervalMillis = 1000 *
+  (+pe.COLLECTOR_HEARTBEAT_INTERVAL_SECS || INTERVAL_DEFAULT);
+if (heartbeatIntervalMillis < FLOOR) {
+  heartbeatIntervalMillis = 1000 * INTERVAL_DEFAULT;
+}
+
+let heartbeatLatencyToleranceMillis = 1000 *
+  +pe.COLLECTOR_HEARTBEAT_LATENCY_TOLERANCE_SECS || LATENCY_DEFAULT;
+if (heartbeatLatencyToleranceMillis < FLOOR) {
+  heartbeatLatencyToleranceMillis = 1000 * LATENCY_DEFAULT;
+}
+
 module.exports = {
   /*
-   * Collector will send a heartbeat every <heartbeatIntervalMillis>
+   * Each collector will send a heartbeat every <heartbeatIntervalMillis>
    * milliseconds.
    */
-  heartbeatIntervalMillis: +pe.COLLECTOR_HEARTBEAT_INTERVAL_MILLIS || 15000,
+  heartbeatIntervalMillis,
+
+  /*
+   * Refocus will treat a collector as down (and try to reassign generators to
+   * a different collector) if (<heartbeatIntervalMillis> +
+   * <heartbeatLatencyToleranceMillis>) milliseconds go by without receiving a
+   * new heartbeat.
+   * For example, let's say heartbeatIntervalMillis=15000 and
+   * heartbeatLatencyToleranceMillis=5000. If a collector's last heartbeat
+   * was at 12:00:00:000 then Refocus will treat the collector as down at 
+   * at 12:00:20:000.
+   */
+  heartbeatLatencyToleranceMillis,
 
   /*
    * Collector will send a batch of samples up to Refocus at least every
