78c6c117c661c7d898e9abdbe3abdbf0576a6755
Ian Goldstein <igoldstein@salesforce.com>
2018-05-04 09:27:22 -0700

    allow admin to prioritize/deprioritize jobs by user name and/or token in addition to just ip address
    modified the calculateJobPriority function signature to get rid of args we aren't using, PLUS added args to pass in conf.prioritizeJobsFrom and conf.deprioritizeJobsFrom for better testability

--- tests/jobQueue/v1/jobWrapper.js
+++ tests/jobQueue/v1/jobWrapper.js
@@ -63,5 +63,87 @@ describe(`tests/jobQueue/v1/jobWrapper.js, api: POST ${path} >`, () => {
     })
     .catch((err) => done(err));
   });
-});
 
+  describe('calculateJobPriority >', () => {
+    const prioritize = [
+      'frodo.baggins@hobbiton.com',
+      '123.456.789',
+      'abcdefg',
+    ];
+    const deprioritize = [
+      'bilbo.baggins@hobbiton.com',
+      '456.789.123',
+      'hijklmnop',
+    ];
+
+    it('prioritize by name', () => {
+      expect(jobWrapper.calculateJobPriority(prioritize, deprioritize, {
+        headers: {
+          UserName: 'frodo.baggins@hobbiton.com',
+          TokenName: 'Smaug',
+          'x-forwarded-for': '123.456.789',
+        },
+      })).to.equal('high');
+    });
+
+    it('prioritize by token', () => {
+      expect(jobWrapper.calculateJobPriority(prioritize, deprioritize, {
+        headers: {
+          UserName: 'legolas@elf.com',
+          TokenName: 'abcdefg',
+          'x-forwarded-for': '456.789.123',
+        },
+      })).to.equal('high');
+    });
+
+    it('prioritize by ip address', () => {
+      expect(jobWrapper.calculateJobPriority(prioritize, deprioritize, {
+        headers: {
+          UserName: 'legolas@elf.com',
+          TokenName: 'Smaug',
+          'x-forwarded-for': '123.456.789',
+        },
+      })).to.equal('high');
+    });
+
+    it('deprioritize by name', () => {
+      expect(jobWrapper.calculateJobPriority(prioritize, deprioritize, {
+        headers: {
+          UserName: 'bilbo.baggins@hobbiton.com',
+          TokenName: 'Smaug',
+          'x-forwarded-for': '789.123.456',
+        },
+      })).to.equal('low');
+    });
+
+    it('deprioritize by token', () => {
+      expect(jobWrapper.calculateJobPriority(prioritize, deprioritize, {
+        headers: {
+          UserName: 'legolas@elf.com',
+          TokenName: 'hijklmnop',
+          'x-forwarded-for': '789.123.456',
+        },
+      })).to.equal('low');
+    });
+
+    it('deprioritize by ip address', () => {
+      expect(jobWrapper.calculateJobPriority(prioritize, deprioritize, {
+        headers: {
+          UserName: 'legolas@elf.com',
+          TokenName: 'Smaug',
+          'x-forwarded-for': '456.789.123',
+        },
+      })).to.equal('low');
+    });
+
+    it('empty prioritize/deprioritize arrays', () => {
+      expect(jobWrapper.calculateJobPriority([], [], {
+        headers: {
+          UserName: 'frodo.baggins@hobbiton.com',
+          TokenName: 'Smaug',
+          'x-forwarded-for': '123.456.789',
+        },
+      })).to.equal('normal');
+    });
+  });
+});
