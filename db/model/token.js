c7dd6c40962e5e2efe4a3d8c765a13e12c5502b8
jgraff2 <jgraff@salesforce.com>
2018-05-29 11:29:57 -0700

    fix associations bug (#863)

--- db/model/token.js
+++ db/model/token.js
@@ -68,6 +68,7 @@ module.exports = function token(seq, dataTypes) {
           include: [
             {
               association: assoc.createdBy,
+              attributes: ['name', 'email'],
             },
           ],
         });
