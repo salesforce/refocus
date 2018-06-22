27e2674808daea1445d454e5aba789fd7b578586
kriscfoster <krisfoster@live.ie>
2018-06-22 11:24:25 +0100

    code style

--- clock/scheduledJobs/deactivateRooms.js
+++ clock/scheduledJobs/deactivateRooms.js
@@ -26,21 +26,21 @@ const THRESHOLD_IN_MINUTES = 120; // 2 hours
  */
 function execute() {
   const deactivateRooms = []; // Keeping track of rooms that were deactivated
-  return dbRoom.findAll({ where: { active: true }})
+  return dbRoom.findAll({ where: { active: true } })
   .then((dbRes) => {
     if (dbRes.length) {
-      return Promise.all(dbRes.map((r) => {
+      return Promise.all(dbRes.map(r => {
         return dbEvent.findAll({ // Finding most recent event for room
           where: { roomId: r.id },
-          order: [[ 'createdAt', 'DESC' ]]
+          order: [['createdAt', 'DESC']],
         })
         .then((evts) => {
-         if (evts.length) {
+          if (evts.length) {
             const minsSinceLastEvent =
               moment().diff(moment(evts[0].createdAt), 'minutes');
             if (minsSinceLastEvent > THRESHOLD_IN_MINUTES) {
-              return r.update({active: false})
-              .then(()=> {
+              return r.update({ active: false })
+              .then(() => {
                 // If a sync is defined then create action
                 if (r.settings && r.settings.sync) {
                   r.settings.sync.forEach((bot) => {
@@ -52,28 +52,24 @@ function execute() {
                       parameters: [],
                     };
 
-                    dbBotAction.create(syncBotAction)
-                  })
+                    dbBotAction.create(syncBotAction);
+                  });
                 }
 
                 deactivateRooms.push(r);
-              })
+              });
             }
           }
-        })
+        });
       })).then(() => {
         // Finished mapping active rooms, return deactivated
-        return Promise.resolve(deactivateRooms)
-      })
+        return Promise.resolve(deactivateRooms);
+      });
     }
 
     // No active rooms
     return Promise.resolve(deactivateRooms);
-  })
-
-
-
-
+  });
 } // execute
 
 module.exports = {
