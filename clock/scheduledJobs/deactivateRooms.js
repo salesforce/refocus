5480908db78a6fa2a5c2c39d3ac2a2d374395908
kriscfoster <krisfoster@live.ie>
2018-06-22 15:21:32 +0100

    use promise.all

--- clock/scheduledJobs/deactivateRooms.js
+++ clock/scheduledJobs/deactivateRooms.js
@@ -20,6 +20,33 @@ const dbEvent = require('../../db/index').Event;
 
 const THRESHOLD_IN_MINUTES = 120; // 2 hours
 
+function checkAndDeactivateRoom(room) {
+  return dbEvent.findOne({
+    where: { roomId: room.id },
+  })
+  .then((evt) => {
+      const minsSinceLastEvent =
+        moment().diff(moment(evt.createdAt), 'minutes');
+      if (minsSinceLastEvent > THRESHOLD_IN_MINUTES) {
+        if (room.settings && room.settings.sync) {
+          room.settings.sync.forEach((bot) => {
+            const syncBotAction = {
+              name: bot.botAction,
+              botId: bot.botId,
+              roomId: room.id,
+              isPending: true,
+              parameters: [],
+            };
+
+            dbBotAction.create(syncBotAction);
+          });
+        }
+
+        return room.update({ active: false })
+      }
+  })
+}
+
 /* For each room that should be deactivated we need to:
  *  - Set active to false
  *  - If sync exists in settings, create sync action
@@ -28,47 +55,11 @@ function execute() {
   const deactivateRooms = []; // Keeping track of rooms that were deactivated
   return dbRoom.findAll({ where: { active: true } })
   .then((dbRes) => {
-    if (dbRes.length) {
-      return Promise.all(dbRes.map(r => {
-        return dbEvent.findAll({ // Finding most recent event for room
-          where: { roomId: r.id },
-          order: [['createdAt', 'DESC']],
-        })
-        .then((evts) => {
-          if (evts.length) {
-            const minsSinceLastEvent =
-              moment().diff(moment(evts[0].createdAt), 'minutes');
-            if (minsSinceLastEvent > THRESHOLD_IN_MINUTES) {
-              return r.update({ active: false })
-              .then(() => {
-                // If a sync is defined then create action
-                if (r.settings && r.settings.sync) {
-                  r.settings.sync.forEach((bot) => {
-                    const syncBotAction = {
-                      name: bot.botAction,
-                      botId: bot.botId,
-                      roomId: r.id,
-                      isPending: true,
-                      parameters: [],
-                    };
-
-                    dbBotAction.create(syncBotAction);
-                  });
-                }
-
-                deactivateRooms.push(r);
-              });
-            }
-          }
-        });
-      })).then(() => {
-        // Finished mapping active rooms, return deactivated
-        return Promise.resolve(deactivateRooms);
-      });
-    }
-
-    // No active rooms
-    return Promise.resolve(deactivateRooms);
+    const promises = dbRes.map((room) => checkAndDeactivateRoom(room));
+    Promise.all(promises)
+    .then((roomsToBeDeactivated) => {
+      return Promise.resolve(roomsToBeDeactivated);
+    });
   });
 } // execute
 
