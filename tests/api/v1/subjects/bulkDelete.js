50c094f6845435e7eb18f5d252cab329dca32725
Cassio Dias <cdias@salesforce.com>
2018-06-23 13:01:48 +0100

    merging test and fixing side effect to DB

--- tests/api/v1/subjects/bulkDelete.js
+++ tests/api/v1/subjects/bulkDelete.js
@@ -1,3 +1,8 @@
+// jscs:disable requirePaddingNewLinesBeforeLineComments
+// jscs:disable maximumLineLength
+// jscs:disable disallowTrailingWhitespace
+/* eslint-disable */
+
 /**
  * Copyright (c) 2018, salesforce.com, inc.
  * All rights reserved.
@@ -15,11 +20,8 @@ const supertest = require('supertest');
 const api = supertest(require('../../../../index').app);
 const testUtils = require('../../../testUtils');
 const Subject = testUtils.db.Subject;
-const Aspect = testUtils.db.Aspect;
-const Sample = testUtils.Sample;
 const utils = require('./utils');
 const constants = require('../../../../api/v1/constants');
-
 const jobQueue = require('../../../../jobQueue/setup').jobQueue;
 const jobType = require('../../../../jobQueue/setup').jobType;
 const bulkDeleteSubjectsJob = require(
@@ -29,123 +31,127 @@ describe('tests/api/v1/subjects/bulkDelete.js', () => {
   let token;
   const AUTHORIZATION = 'Authorization';
   const DELETE_PATH = '/v1/subjects/delete/bulk';
-  let northAmericaId = 0;
-  let southAmericaId = 0;
-  let europeId = 0;
-  let northAmericaName = '';
+
+  let parentAndChildIdStore = [];
+  let fooAndBlahIdStore = [];
 
   before((done) => {
-    // Start JobQueue
-    jobQueue.process(jobType.BULK_DELETE_SUBJECTS, bulkDeleteSubjectsJob);
-
     testUtils.createToken()
       .then((returnedToken) => {
         token = returnedToken;
         done();
-      })
-      .catch(done);
+      }).catch(done);
   });
 
-  describe('Create 3 subjects and delete two', () => {
-    before((done) => {
-      northAmericaName = `${testUtils.namePrefix}NorthAmerica`;
-      const subjectNorthAmerica = { name: northAmericaName, isPublished: true,
-        absolutePath: northAmericaName, };
-      const subjectSouthAmerica = { name: `${testUtils.namePrefix}SouthAmerica`,
-        isPublished: false, };
-      const subjectEurope = { name: `${testUtils.namePrefix}Europe`,
-        isPublished: false, };
-      const aspectTemperature = { name: 'temperature', timeout: '30s',
-        isPublished: true, tags: ['temp'], };
-      const aspectHumid = { name: 'humidity', timeout: '30s',
-        isPublished: true, };
-      const sample1 = { value: '10' };
-      const sample2 = { value: '10' };
+  describe('Create subjects and check status', () => {
 
-      Subject.create(subjectNorthAmerica)
-        .then((createdSubjectNorthAmerica) => {
-          sample1.subjectId = createdSubjectNorthAmerica.id;
-          sample2.subjectId = createdSubjectNorthAmerica.id;
-          northAmericaId = createdSubjectNorthAmerica.id;
-          return Subject.create(subjectSouthAmerica);
-        })
-        .then((createdSubjectSouthAmerica) => {
-          southAmericaId = createdSubjectSouthAmerica.id;
-          return Subject.create(subjectEurope);
-        })
-        .then((createdEurope) => {
-          europeId = createdEurope.id;
-          return Aspect.create(aspectHumid);
-        })
-        .then((createdAspectHumid) => {
-          sample1.aspectId = createdAspectHumid.id;
-          return Aspect.create(aspectTemperature);
-        })
-        .then((createdAspectTemperature) => {
-          sample2.aspectId = createdAspectTemperature.id;
-          return Sample.create(sample1);
-        })
-        .then(() => Sample.create(sample2))
-        .then(() => done())
-        .catch(done);
-    });
-    before(utils.populateRedis);
+    // Start JobQueue
+    jobQueue.process(jobType.BULK_DELETE_SUBJECTS, bulkDeleteSubjectsJob);
 
-    after(utils.forceDelete);
+    before(()=> testUtils.toggleOverride('enableWorkerProcess', true));
+
+    beforeEach(utils.populateRedis);
+    afterEach(utils.forceDelete);
+
     after(testUtils.forceDeleteUser);
+    after(() => testUtils.toggleOverride('enableWorkerProcess', false));
 
-    it('Must throw bad request when send invalid keys', (done) => {
-      api.post(DELETE_PATH)
-        .set(AUTHORIZATION, token)
-        .send('blah')
-        .expect(constants.httpStatus.BAD_REQUEST)
-        .then(() => done());
+    // Create parent and child subject (not able to delete)
+    before((done) => {
+      const subjectParent = { name: `${testUtils.namePrefix}ParentName`, isPublished: true, childCount: 1, };
+      const subjectChild = { name: `${testUtils.namePrefix}ChildName`, isPublished: true, childCount: 0, };
+      Subject.create(subjectParent)
+        .then((createdSubjectParent) => {
+          subjectChild.parentId = createdSubjectParent.id;
+          parentAndChildIdStore.push(createdSubjectParent.id);
+          return Subject.create(subjectChild);
+        })
+        .then((createdSubjectChild) => {
+          parentAndChildIdStore.push(createdSubjectChild.id);
+          done();
+        }).catch(done);
     });
 
-    it('Must be able to delete subject N.America by path and EU by ID ' +
-      'retrieving current JobId', (done) => {
+    // Create not related subjects (able to delete)
+    before((done) => {
+      const blah = { name: `${testUtils.namePrefix}Blah`, isPublished: true, };
+      const foo = { name: `${testUtils.namePrefix}Foo`, isPublished: true, };
+      Subject.create(blah)
+        .then((createdBlah) => {
+          fooAndBlahIdStore.push(createdBlah.id);
+          return Subject.create(foo);
+        })
+        .then((createdFoo) => {
+          fooAndBlahIdStore.push(createdFoo.id);
+          done();
+        }).catch(done);
+    });
+
+    it('Must show status with SubjectDeleteConstraintError when delete subject with child', (done) => {
       api.post(DELETE_PATH)
         .set(AUTHORIZATION, token)
-        .send([northAmericaName, europeId])
+        .send(parentAndChildIdStore)
         .expect(constants.httpStatus.OK)
-        .then((res) => expect(res.body.jobId).to.not.be.empty)
-        .then(() => {
-          // postgres must have '___SouthAmerica'
+        .then((res) => {
+          expect(res.body.jobId).to.not.be.empty;
+          return Promise.resolve(res.body.jobId);
+        })
+        .then((jobId) => {
           setTimeout(() => {
-            Subject.findAll()
-              .then((res) => {
-                expect(res).to.have.length(1);
-                expect(res[0].absolutePath).to.equal('___SouthAmerica');
-                return done();
-              });
-          }, 100);
-        }).then(() => {
-          // Checking via API...
-          setTimeout(() => {
-            // Must not find north america
-            api.get(`/v1/subjects/${northAmericaId}`)
-              .set('Authorization', token)
-              .expect(constants.httpStatus.NOT_FOUND)
-              .then((res) => {
-                expect(res.body.errors[0].type).to
-                  .equal('ResourceNotFoundError');
-              });
-
-            // Must not find europe
-            api.get(`/v1/subjects/${europeId}`)
-              .set('Authorization', token)
-              .expect(constants.httpStatus.NOT_FOUND)
-              .then((res) => {
-                expect(res.body.errors[0].type).to
-                  .equal('ResourceNotFoundError');
-              });
-
-            // Must find south america
-            api.get(`/v1/subjects/${southAmericaId}`)
+            // Job status must show the error
+            api.get(`/v1/subjects/delete/bulk/${jobId}/status`)
               .set('Authorization', token)
               .expect(constants.httpStatus.OK)
               .then((res) => {
-                expect(res.body.id).to.equal(southAmericaId);
+                expect(res.body.errors).to.not.be.empty;
+                expect(res.body.errors[0].name).to.equal('SubjectDeleteConstraintError');
+                return done();
+              });
+
+          }, 100);
+        });
+    });
+
+    it('Must show complete after delete successfully 2 subjects', (done) => {
+      api.post(DELETE_PATH)
+        .set(AUTHORIZATION, token)
+        .send(fooAndBlahIdStore)
+        .expect(constants.httpStatus.OK)
+        .then((res) => {
+          expect(res.body.jobId).to.not.be.empty;
+          return Promise.resolve(res.body.jobId);
+        })
+        .then((jobId) => {
+          setTimeout(() => {
+            // Job complete
+            api.get(`/v1/subjects/delete/bulk/${jobId}/status`)
+              .set('Authorization', token)
+              .expect(constants.httpStatus.OK)
+              .then((res) => {
+                expect(res.body).to.not.be.empty;
+                expect(res.body.status).to.equal('complete');
+                return done();
+              });
+          }, 100);
+        })
+        .then(() => {
+          setTimeout(() => {
+            // Must not find Blah
+            api.get(`/v1/subjects/${fooAndBlahIdStore[0]}`)
+              .set('Authorization', token)
+              .expect(constants.httpStatus.NOT_FOUND)
+              .then((res) => {
+                expect(res.body.errors[0].type).to
+                  .equal('ResourceNotFoundError');
+              });
+
+            // Must not find Foo
+            api.get(`/v1/subjects/${fooAndBlahIdStore[1]}`)
+              .set('Authorization', token)
+              .expect(constants.httpStatus.NOT_FOUND)
+              .then((res) => {
+                expect(res.body.errors[0].type).to
+                  .equal('ResourceNotFoundError');
               });
           }, 100);
         });
