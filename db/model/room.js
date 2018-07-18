salesforce/refocus

commit 3c4cba65c2069fd854fa7ec65c330eeeb66f58a8

minor perspective/room namespace initialization optimization (#953)

db/model/room.js
@@ -176,6 +176,13 @@ module.exports = function room(seq, dataTypes) {
       through: 'RoomWriters',
       foreignKey: 'roomId',
     });
+    Room.addScope('namespace', {
+      attributes: ['id', 'name'],
+      where: {
+        active: true,
+        isDeleted: 0,
+      },
+    });
   };
 
   return Room;

db/model/perspective.js
@@ -220,6 +220,24 @@ module.exports = function perspective(seq, dataTypes) {
       ],
     });
 
+    Perspective.addScope('namespace', {
+      attributes: [
+        'id',
+        'name',
+        'rootSubject',
+        'aspectFilterType',
+        'aspectFilter',
+        'aspectTagFilterType',
+        'aspectTagFilter',
+        'subjectTagFilterType',
+        'subjectTagFilter',
+        'statusFilterType',
+        'statusFilter',
+      ],
+      where: {
+        isDeleted: 0,
+      },
+    });
   };
 
   /**

realtime/setupSocketIO.js
@@ -59,28 +59,11 @@ function getUserFromSession(sid, redisStore) {
  *  server-side object with the namespace initialized.
  */
 function setupNamespace(io) {
-  return new Promise((resolve, reject) => {
-    perspective.findAll()
-    .then((objArr) => {
-      if (objArr) {
-        objArr.forEach((o) => rtUtils.initializePerspectiveNamespace(o, io));
-      }
-    })
-    .then(() => room.findAll(
-      {
-        where: {
-          active: true,
-        },
-      })
-      .then((rooms) => {
-        if (rooms) {
-          rooms.forEach((r) => rtUtils.initializeBotNamespace(r.toJSON(), io));
-          resolve(io);
-        }
-      })
-    )
-    .catch(reject);
-  });
+  const perspPromise = perspective.scope('namespace').findAll()
+    .map((p) => rtUtils.initializePerspectiveNamespace(p, io));
+  const roomPromise = room.scope('namespace').findAll()
+    .map((r) => rtUtils.initializeBotNamespace(r, io));
+  return Promise.all([perspPromise, roomPromise]);
 } // setupNamespace
 
 /**

commit 87ff6ce86042a511836ffe7e3a246128a567d72f

Fixes bug introduced in https://github.com/salesforce/refocus/pull/953: (#960)

db/model/room.js
@@ -180,7 +180,6 @@ module.exports = function room(seq, dataTypes) {
       attributes: ['id', 'name'],
       where: {
         active: true,
-        isDeleted: 0,
       },
     });
   };