93999d4d4e73e3835e8bdabed2d323ad9105ba4b
kriscfoster <krisfoster@live.ie>
2018-07-19 16:14:31 +0100

    revert

--- tests/ui/pages/login.js
+++ tests/ui/pages/login.js
@@ -1,24 +1,14 @@
 const expect = require('chai').expect;
-const puppeteer = require('puppeteer');
-const sinon = require('sinon');
-const authenticate = require('../../../api/v1/controllers/authenticate.js');
-
 const setup = require('../setup.js');
+const { baseUrl } = setup;
+
 let browser;
 
-const opts = {
-  headless: true,
-  args:['--no-sandbox', '--disable-setuid-sandbox'],
-  slowMo: 80,
-  timeout: 10000
-};
-
 before ((done) => {
-  puppeteer.launch(opts)
-    .then((brws) => {
-      browser = brws;
-      done();
-    });
+  setup.puppeteer().then((bwr) => {
+    browser = bwr;
+    done();
+  })
 });
 
 after (() => {
@@ -26,57 +16,40 @@ after (() => {
 });
 
 describe('sample test', function() {
-  //this.timeout(1000000);
+  this.timeout(1000000);
   let page;
-
-  before(() => {
-    // postRequest = sinon.stub(authenticate, 'authenticateUser');
-  })
-
-  after(() => {
-    // postRequest.restore();
-  })
 
   beforeEach (async () => {
     page = await browser.newPage();
-    await page.goto('http://localhost:3000/login');
+    await page.goto(baseUrl + '/login');
   });
 
   afterEach (async () => {
     await page.close();
   });
 
-  it('ok, should have a heading', async function () {
-    console.log('TEST HAPPENING HERE');
-    const HEADING_SELECTOR = 'h1';
-    let heading;
-    await page.waitFor(HEADING_SELECTOR);
-    heading = await page.$eval(HEADING_SELECTOR, heading => heading.innerText);
-    expect(heading).to.eql('Refocus');
+  it('ok, /login componants load and work', async function () {
+    await page.waitFor('h1');
+    expect(await page.$eval('h1', heading => heading.innerText)).to.eql('Refocus');
+    await page.waitForSelector("input[name=username]");
+    expect(await page.$eval('[name=username]', input => input.placeholder)).to.eql('Enter username');
+    await page.click("input[name=username]");
+    await page.type("input[name=username]", 'potatoes');
+    expect(await page.$eval('[name=username]', input => input.value)).to.eql('potatoes');
   });
 
-  // it('ok, should be able to enter username', async () => {
-  //   postRequest.returns('aaa');
-  //   console.log(authenticate.authenticateUser());
+//   it('ok, /logging in', async function () {
+//     await page.waitFor('h1');
+//     expect(await page.$eval('h1', heading => heading.innerText)).to.eql('Refocus');
+//     await page.waitForSelector("input[name=username]");
+//     expect(await page.$eval('[name=username]', input => input.placeholder)).to.eql('Enter username');
+//     await page.click("input[name=username]");
+//     await page.type("input[name=username]", 'admin@refocus.admin');
+//     await page.click("input[name=password]");
+//     await page.type("input[name=password]", 'password');
+//     await page.click("button[type=submit]");
+//     await page.waitForNavigation();
+// +    expect(page.url()).to.equal('http://localhost:3000/perspectives');
+//   });
 
-  //   await page.waitForSelector("input[name=username]");
-  //   await page.click("input[name=username]")
-  //   await page.type("input[name=username]", 'username');
-  //   await page.click("input[name=password]")
-  //   await page.type("input[name=password]", 'password');
-  //   await page.click('button[type=submit]');
-  //   await page.waitForNavigation();
-  //   expect(page.url()).to.equal('http://localhost:3000/perspectives');
-  // });
-
-  // it('fail, incorrect password', async () => {
-  //   await page.waitForSelector("input[name=username]");
-  //   await page.click("input[name=username]")
-  //   await page.type("input[name=username]", 'admin@refocus.admin');
-  //   await page.click("input[name=password]")
-  //   await page.type("input[name=password]", 'incorrect');
-  //   await page.click('button[type=submit]');
-  //   await page.waitForNavigation();
-  //   expect(page.url()).to.equal('http://localhost:3000/login');
-  // });
 });
--- tests/ui/setup.js
+++ tests/ui/setup.js
@@ -1,1 +1,25 @@
+const puppeteer = require('puppeteer');
+const sinon = require('sinon');
+
+//const authenticate = require('../../api/v1/controllers/authenticate');
+//const authStub = sinon.stub(authenticate, 'authenticateUser')
+//authStub.returns('aaa');
+//authStub.callsArg(2);
+//console.log(authenticate.authenticateUser())
+
 const app = require('./../../index.js').app;
+
+const opts = {
+  headless: false,
+  slowMo: 10,
+  timeout: 10000,
+  //args:['--no-sandbox', '--disable-setuid-sandbox']
+};
+
+module.exports = {
+  baseUrl: 'http://localhost:3000',
+  puppeteer() {
+    // Promise which returns a browser object
+    return puppeteer.launch(opts);
+  },
+}
