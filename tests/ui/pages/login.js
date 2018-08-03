220522865292da026458ea3dc000aed912a6e6de
Kris Foster <kfoster@salesforce.com>
2018-08-03 17:17:24 +0100

    comments

--- tests/ui/pages/login.js
+++ tests/ui/pages/login.js
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
+
+/**
+ * tests/ui/pages/login.js
+ */
 const expect = require('chai').expect;
 const setup = require('../setup.js');
 const testUtils = require('../../testUtils.js');
--- tests/ui/pages/rooms/list.js
+++ tests/ui/pages/rooms/list.js
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
+
+/**
+ * tests/ui/pages/rooms/list.js
+ */
 const expect = require('chai').expect;
 
 const utils = require('../../utils.js');
