9a16d5c9d0bafa62a396ab1efa08fa6b91afbe33
kriscfoster <krisfoster@live.ie>
2018-08-17 10:25:55 +0100

    Filter events by event type (#981)
    * where clause working
    
    * filtering
    
    * test
    
    * bug fix
    
    * comment fix
    
    * extra quote
    
    * closing comma
    
    * fixed
    
    * case insensitive test

--- tests/api/v1/events/get.js
+++ tests/api/v1/events/get.js
@@ -33,8 +33,10 @@ describe('tests/api/v1/events/get.js >', () => {
   let testEvent = u.getStandard();
   let testEventOutput;
   let testEvent2 = u.getStandard();
+  testEvent2.context.type = 'Comment';
   testEvent2.log = 'Sample Event 2';
   let testEvent3 = u.getStandard();
+  testEvent3.context.type = 'Comment';
   testEvent3.log = 'Sample Event 3';
   let token;
 
@@ -228,6 +230,34 @@ describe('tests/api/v1/events/get.js >', () => {
       }
 
       expect(res.body.log).to.equal(u.log);
+      done();
+    });
+  });
+
+  it('Pass, get by context type', (done) => {
+    api.get(`${path}?type=Comment`)
+    .set('Authorization', token)
+    .expect(constants.httpStatus.OK)
+    .end((err, res) => {
+      if (err) {
+        return done(err);
+      }
+
+      expect(res.body.length).to.equal(TWO);
+      done();
+    });
+  });
+
+  it('Pass, get by context type is not case sensitive', (done) => {
+    api.get(`${path}?type=comment`)
+    .set('Authorization', token)
+    .expect(constants.httpStatus.OK)
+    .end((err, res) => {
+      if (err) {
+        return done(err);
+      }
+
+      expect(res.body.length).to.equal(TWO);
       done();
     });
   });
