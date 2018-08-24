eac3c3a50d7949c653bd53749d5c05baf5871f03
Ian Goldstein <igoldstein@salesforce.com>
2018-08-24 09:08:55 -0700

    no message

--- tests/utils/logEnvVars.js
+++ tests/utils/logEnvVars.js
@@ -14,70 +14,61 @@ const expect = require('chai').expect;
 const logEnvVars = require('../../utils/logEnvVars');
 
 describe('tests/utils/logEnvVars.js >', () => {
+  describe('truncate >', () => {
+    it('over the maxlen, truncated', () => {
+      expect(logEnvVars.truncate('abcdefgh', 6)).to.equal('abc...');
+    });
+
+    it('equal maxlen', () => {
+      expect(logEnvVars.truncate('abcdef', 6)).to.equal('abcdef');
+    });
+
+    it('under maxlen', () => {
+      expect(logEnvVars.truncate('abc', 6)).to.equal('abc');
+    });
+
+    it('false-y strings', () => {
+      expect(logEnvVars.truncate('', 5)).to.equal('');
+      expect(logEnvVars.truncate(null, 5)).to.equal('');
+      expect(logEnvVars.truncate(undefined, 5)).to.equal('');
+    });
+  });
+
   describe('prepareObjectsToLog >', () => {
-    it('hides values for exact (case-sensitive) match of key name for ' +
-    'hidden keys', () => {
-      const data = {
-        SECRET_TOKEN: 'This is a SECRET_TOKEN, should be hidden',
-        SESSION_SECRET: 'This is a SESSION_SECRET, should be hidden',
-        SECURITYSESSIONID: 'This is a SECURITYSESSIONID, should be hidden',
-        SECRET_TOKEN1: '1',
-        SESSION_SECRET2: '2',
-        SECURITYSESSIONID3: '3',
-        '4SECRET_TOKEN': '4',
-        '5SESSION_SECRET': '5',
-        '6SECURITYSESSIONID': '6',
-        _SECRET_TOKEN: '7',
-        _SESSION_SECRET2: '8',
-        _SECURITYSESSIONID3: '9',
-        SECRET_Token: '10',
-        SESSION_Secret: '11',
-        SecuritySESSIONId: '12',
+    describe('with mask list >', () => {
+      const opts = {
+        MASK_LIST: 'ABC,DEF',
       };
-      expect(logEnvVars.prepareObjectsToLog(data)).to.deep.equal([
-        { name: '4SECRET_TOKEN', value: '"4"' },
-        { name: '5SESSION_SECRET', value: '"5"' },
-        { name: '6SECURITYSESSIONID', value: '"6"' },
-        { name: 'SECRET_TOKEN', value: '"hidden"' },
-        { name: 'SECRET_TOKEN1', value: '"1"' },
-        { name: 'SECRET_Token', value: '"10"' },
-        { name: 'SECURITYSESSIONID', value: '"hidden"' },
-        { name: 'SECURITYSESSIONID3', value: '"3"' },
-        { name: 'SESSION_SECRET', value: '"hidden"' },
-        { name: 'SESSION_SECRET2', value: '"2"' },
-        { name: 'SESSION_Secret', value: '"11"' },
-        { name: 'SecuritySESSIONId', value: '"12"' },
-        { name: '_SECRET_TOKEN', value: '"7"' },
-        { name: '_SECURITYSESSIONID3', value: '"9"' },
-        { name: '_SESSION_SECRET2', value: '"8"' },
-      ]);
-    });
 
-    it('skips keys starting with HEROKU_ or REDIS_BASTION or npm_, ' +
-    'case sensitive', () => {
-      const data = {
-        HEROKU_: '1',
-        HEROKU_2: '2',
-        Heroku_3: '3',
-        REDIS_BASTION: '4',
-        REDIS_BASTION5: '5',
-        Redis_BASTION6: '6',
-        npm_7: '7',
-        NPM_8: '8',
-        npm_: '9',
-      };
-      expect(logEnvVars.prepareObjectsToLog(data)).to.deep.equal([
-        { name: 'Heroku_3', value: '"3"' },
-        { name: 'NPM_8', value: '"8"' },
-        { name: 'Redis_BASTION6', value: '"6"' },
-      ]);
-    });
+      it('hides exact, case-sensitive matches', () => {
+        const data = {
+          ABC: '1',
+          _ABC: '2',
+          ABC_: '3',
+          $ABC: '4',
+          abc: '5',
+          ab: '6',
+          DEF: '7',
+        };
+        expect(logEnvVars.prepareObjectsToLog(data, opts)).to.deep.equal([
+          { name: '$ABC', value: '"4"' },
+          { name: 'ABC', value: '"hidden"' },
+          { name: 'ABC_', value: '"3"' },
+          { name: 'DEF', value: '"hidden"' },
+          { name: '_ABC', value: '"2"' },
+          { name: 'ab', value: '"6"' },
+          { name: 'abc', value: '"5"' },
+        ]);
+      });
 
-    it('unexpected or empty env returns empty array', () => {
-      expect(logEnvVars.prepareObjectsToLog({})).to.deep.equal([]);
-      expect(logEnvVars.prepareObjectsToLog(undefined)).to.deep.equal([]);
-      expect(logEnvVars.prepareObjectsToLog(null)).to.deep.equal([]);
-      expect(logEnvVars.prepareObjectsToLog([1, 2, 'abc'])).to.deep.equal([]);
+      it('unexpected or empty env returns empty array', () => {
+        expect(logEnvVars.prepareObjectsToLog({}, opts)).to.deep.equal([]);
+        expect(logEnvVars.prepareObjectsToLog(undefined, opts))
+        .to.deep.equal([]);
+        expect(logEnvVars.prepareObjectsToLog(null, opts)).to.deep.equal([]);
+        expect(logEnvVars.prepareObjectsToLog([1, 2, 'abc'], opts))
+        .to.deep.equal([]);
+      });
     });
   });
 });
--- utils/logEnvVars.js
+++ utils/logEnvVars.js
@@ -11,31 +11,68 @@
  */
 'use strict'; // eslint-disable-line strict
 const featureToggles = require('feature-toggles');
+const config = require('../config');
 const activityLog = require('./activityLog');
-const HIDE_ME_LIST = [
-  'DATABASE_BASTION_KEY',
-  'DATABASE_URL',
-  'REDIS_URL',
-  'SECRET_TOKEN',
-  'SESSION_SECRET',
-  'SECURITYSESSIONID',
-];
-const SKIP_ME_REGEX = /^(HEROKU_|REDIS_BASTION|npm_).*/;
+
+ /**
+  * Note: with node 8, assigning a property on process.env will implicitly
+  * convert the value to a string so we do not need to worry about non-string
+  * types here.
+  */
+function truncate(str = '', max) {
+  if (!str) return '';
+  if (str.length > max) {
+    str = str.substring(0, max - 3) + '...';
+  }
+
+  return str;
+} // truncate
+
+/**
+ * Set up the config options, starting from the values from config OR using
+ * override values from opts arg.
+ *
+ * @param {Object} conf - options from config.logEnvVars
+ * @param {Object} opts - options object containing optional MASK_LIST and
+ *  MAX_LEN attributes
+ * @returns {Object} config options
+ */
+function prepareConfOpts(conf, opts) {
+  const retval = {
+    maskList: conf.MASK_LIST,
+    maxValueLength: conf.MAX_LEN,
+  };
+
+  if (opts && opts.MAX_LEN) retval.maxValueLength = opts.MAX_LEN;
+  if (retval.maxValueLength < 4) retval.maxValueLength = 512;
+  if (opts && opts.MASK_LIST) retval.maskList = opts.MASK_LIST;
+  return retval;
+} // prepareConfOpts
 
 /**
  * Returns array of 'env' activity log objects, or returns an empty array if
  * env is not an object or is false-y or is an array.
+ *
+ * @param {Object} env - will be process.env, but exposing it as arg to make it
+ *  easier to test (assumes it has typeof "object")
+ * @param {Object} opts - will be config.logEnvVars, but exposing it as arg to
+ *  make it easier to test
  */
-function prepareObjectsToLog(env) {
-  if (env && Array.isArray(env)) return [];
+function prepareObjectsToLog(env, opts) {
+  /* Short circuit - do nothing if env is false-y or if it's an array. */
+  if (!env || Array.isArray(env)) return []; 
 
+  opts = prepareConfOpts(config.logEnvVars, opts);
   return Object.keys(env || {})
-  .filter((name) => !name.match(SKIP_ME_REGEX))
   .sort()
-  .map((name) => ({
-    name,
-    value: HIDE_ME_LIST.includes(name) ? '"hidden"' : `"${env[name]}"`,
-  }));
+  .map((name) => {
+    const val = opts.maskList.includes(name) ? 'hidden' :
+      truncate(env[name], opts.maxValueLength);
+    return {
+      name,
+      value: `"${val}"`,
+    };
+  });
 } // prepareObjectsToLog
 
 function log(env) {
@@ -47,5 +84,7 @@ function log(env) {
 
 module.exports = {
   log,
+  prepareConfOpts, // for testing only
   prepareObjectsToLog, // for testing only
+  truncate, // for testing only
 };
