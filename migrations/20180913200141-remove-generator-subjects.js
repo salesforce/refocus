9cc320c8224132b6652637f43dc342c1ebe166b3
jgraff <jgraff@salesforce.com>
2018-09-17 14:16:16 -0700

    code style

--- migrations/20180913200141-remove-generator-subjects.js
+++ migrations/20180913200141-remove-generator-subjects.js
@@ -2,12 +2,12 @@
 require('../db/constants');
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
+  up(queryInterface, Sequelize) {
     return queryInterface.removeColumn('Generators', 'subjects');
   },
 
-  down: (queryInterface, Sequelize) => {
+  down(queryInterface, Sequelize) {
     const type = Sequelize.ARRAY(Sequelize.STRING(constants.fieldlen.normalName));
     return queryInterface.addColumn('Generators', 'subjects', { type });
-  }
+  },
 };
