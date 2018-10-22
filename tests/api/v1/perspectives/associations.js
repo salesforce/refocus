8fc92a1c6f9ef4004a5352ec5a251001ac1ad8e4
Cassio Dias <cdias@salesforce.com>
2018-10-22 14:07:05 +0100

    adding test for extra FK when needed

--- tests/api/v1/perspectives/associations.js
+++ tests/api/v1/perspectives/associations.js
@@ -98,7 +98,7 @@ describe(`tests/api/v1/perspectives/associations.js, GET ${path} >`, () => {
             associations.forEach((association) => {
               expect(record).to.have.property(association);
               /*
-              API is returning extra fields as a solution for Sequelize inner
+               API is returning extra fields as a solution for Sequelize inner
               query issue that doesn't not return FK when multiple associations.
               */
               expect(record).to.have.property('lensId');
