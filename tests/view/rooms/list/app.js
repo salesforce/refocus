63968ef7aff707c9b93936d131815405f011070d
kriscfoster <krisfoster@live.ie>
2018-10-23 14:37:09 +0100

    simple tests added

---
+++ tests/view/rooms/list/app.js
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
+
+/**
+ * tests/view/rooms/list/app.js
+ */
+
+import ReactTestUtils from 'react-dom/test-utils';
+
+const expect = require('chai').expect;
+const app = require('../../../../view/rooms/list/app.js');
+
+describe('tests/view/rooms/list/app.js', () => {
+  const testArr = ['1', '2', '3'];
+
+  it('ok, select element created with 3 options', () => {
+    const selectEl = app.createSelectEl(testArr)
+    expect(selectEl.tagName).to.equal('SELECT');
+    expect(selectEl.childElementCount).to.equal(testArr.length);
+    expect(selectEl.value).to.equal('1');
+  });
+
+  it('ok, select element created and default value is overwritten', () => {
+    const selectEl = app.createSelectEl(testArr, '2')
+    expect(selectEl.value).to.equal('2');
+  });
+
+  it('ok, constructing url after page filter change', () => {
+    const url = app.constructListFilterUrl('page', '3');
+    expect(url).to.equal('/rooms?page=3');
+  });
+
+  it('ok, constructing url after type filter change (page is reset to 1)', () => {
+    const url = app.constructListFilterUrl('type', 'coolName');
+    expect(url).to.equal('/rooms?page=1&type=coolName');
+  });
+
+  it('ok, constructing url after active filter change (page is reset to 1)', () => {
+    const url = app.constructListFilterUrl('active', 'false');
+    expect(url).to.equal('/rooms?page=1&active=false');
+  });
+});
+
