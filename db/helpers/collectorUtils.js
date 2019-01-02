812eae88b43d7b5e6b13b45b96a5f2ba9a877984
Cassio Dias <cdias@salesforce.com>
2019-01-02 18:09:45 +0000

    Collector group create (#1059)

--- db/helpers/collectorUtils.js
+++ db/helpers/collectorUtils.js
@@ -15,7 +15,10 @@
 'use strict'; // eslint-disable-line strict
 
 const Joi = require('joi');
-const ValidationError = require('../dbErrors').ValidationError;
+const common = require('./common');
+const dbErrors = require('../dbErrors');
+const ValidationError = dbErrors.ValidationError;
+const Op = require('sequelize').Op;
 const semverRegex = require('semver-regex');
 const utils = require('../utils.js');
 const featureToggles = require('feature-toggles');
@@ -85,6 +88,22 @@ function validateVersion(version) {
 }
 
 /**
+ * Reject the request if collectorNames contain duplicate names
+ * @param {Array} collectorNames Array of strings
+ * @returns {Promise} empty if validation passed, reject otherwise
+ */
+function validateNames(collectorNames) {
+  if (common.checkDuplicatesInStringArray(collectorNames)) {
+    const err = new dbErrors.DuplicateCollectorError();
+    err.resourceType = 'Collector';
+    err.resourceKey = collectorNames;
+    return Promise.reject(err);
+  }
+
+  return Promise.resolve();
+}
+
+/**
  * Find unassigned generators and assign them.
  * @returns {Promise} - Resolves to array of assigned generator db objects
  */
@@ -108,9 +127,81 @@ function assignUnassignedGenerators() {
   });
 }
 
+/**
+ * Returns a where clause object that uses the "IN" operator
+ * @param  {Array} arr - An array that needs to be assigned to the "IN" operator
+ * @returns {Object} - An where clause object
+ */
+function whereClauseForNameInArr(arr) {
+  const whr = {};
+  whr.name = {};
+  whr.name[Op.in] = arr;
+  return whr;
+} // whereClauseForNameInArr
+
+/**
+ * If collectors exist, return a Promise with an
+ * Array of collector objects referenced by collectorNames.
+ * If collector names are not supplied, return a Promise
+ * with an empty array
+ * If collector names are invalid, reject with error.
+
+ * @param {Object} seq The sequelize object
+ * @param {Array} collectorNames Array of Strings
+ * @returns {Promise} with an array if check passed, error otherwise
+ */
+function getByNames(seq, collectorNames) {
+  if (!collectorNames || !collectorNames.length) return [];
+
+  const options = {};
+  options.where = whereClauseForNameInArr(collectorNames);
+
+  // reject the request if collectorNames contain duplicate names
+  return new Promise((resolve, reject) =>
+    seq.models.Collector.findAll(options)
+      .then((_collectors) => {
+        /*
+         * If requestBody does not have a collectors field, OR
+         * if the number of collectors in requestBody MATCH the
+         * GET result, order the collectors AND create the generator.
+         * Else throw error since there are collectors that don't exist.
+         */
+        if (_collectors.length === collectorNames.length) {
+          resolve(_collectors);
+        }
+
+        const err = new dbErrors.ResourceNotFoundError();
+        err.resourceType = 'Collector';
+        err.resourceKey = collectorNames;
+        reject(err);
+      })
+  );
+}
+
+/**
+ * Used by db model.
+ * Validate the collectors field: if succeed, return a promise with
+ * the collectors.
+ * If fail, reject Promise with the appropriate error
+ *
+ * @param {Object} seq the Sequelize object
+ * @param {Array} collectorNames Array of strings
+ * @returns {Promise} with collectors if validation and check pass,
+ * rejected promise with the appropriate error otherwise.
+ */
+function validate(seq, collectorNames) {
+  return new seq.Promise((resolve, reject) =>
+    validateNames(collectorNames)
+      .then(() => getByNames(seq, collectorNames))
+      .then(resolve)
+      .catch(reject)
+  );
+}
+
 module.exports = {
   validateOsInfo,
   validateProcessInfo,
   validateVersion,
   assignUnassignedGenerators,
+  validate,
 }; // exports
--- db/model/collectorgroup.js
+++ db/model/collectorgroup.js
@@ -11,7 +11,9 @@
  */
 
 const common = require('../helpers/common');
+const collectorUtils = require('../helpers/collectorUtils');
 const constants = require('../constants');
+const dbUtils = require('../utils');
 const assoc = {};
 
 module.exports = function collectorgroup(seq, dataTypes) {
@@ -86,21 +88,32 @@ module.exports = function collectorgroup(seq, dataTypes) {
       foreignKey: 'createdBy',
     });
 
-    CollectorGroup.addScope('defaultScope', {
+    CollectorGroup.addScope('baseScope', {
+      order: ['name'],
+    });
+
+    CollectorGroup.addScope('collectors', {
       include: [
         {
           model: models.Collector.scope('embed'),
           as: 'collectors',
+          required: false,
         },
       ],
-      order: ['name'],
-    }, { override: true });
+    });
+
+    CollectorGroup.addScope('defaultScope',
+      dbUtils.combineScopes([
+        'baseScope',
+        'collectors',
+      ], CollectorGroup),
+      { override: true }
+    );
   };
 
   /**
    * Instance Methods:
    */
-
   CollectorGroup.prototype.isWritableBy = function (who) {
     return new seq.Promise((resolve /* , reject */) =>
       this.getWriters()
@@ -114,6 +127,36 @@ module.exports = function collectorgroup(seq, dataTypes) {
         resolve(found.length === 1);
       }));
   }; // isWritableBy
+
+  CollectorGroup.createCollectorGroup = function (requestBody) {
+    let collectors;
+
+    return Promise.resolve()
+      .then(() => {
+        if (!(requestBody.collectors && requestBody.collectors.length > 0)) {
+          return [];
+        }
+
+        return collectorUtils
+          .validate(seq, requestBody.collectors)
+          .then((validCollectors) => {
+            const alreadyAssigned = validCollectors
+              .filter((collector) => collector.collectorGroupId);
+
+            if (alreadyAssigned && alreadyAssigned.length > 0) {
+              const names = alreadyAssigned.map((c) => c.name);
+              const msg = 'Collector ' + names +
+                ' already assigned to a different group';
+              throw new Error(msg);
+            }
+
+            collectors = validCollectors;
+          });
+      })
+      .then(() => CollectorGroup.create(requestBody))
+      .then((collectorGroup) => collectorGroup.setCollectors(collectors))
+      .then((collectorGroup) => collectorGroup.reload());
+  };
 
   return CollectorGroup;
 };
