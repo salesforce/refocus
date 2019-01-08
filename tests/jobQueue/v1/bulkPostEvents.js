c6e61e74bd65103c8758e9495e2ecd14b7a86b52
Kris foster <krisfoster@live.ie>
2019-01-08 21:25:57 +0000

    Bulk POST Events (#1065)
    * bulk upsert events
    
    * bulk test
    
    * tidy
    
    * clean
    
    * remove unneccessary
    
    * jsdoc
    
    * style
    
    * jcs errors
    
    * working for non-job
    
    * without worker process
    
    * bulk create events
    
    * bulk create
    
    * remove unneeded
    
    * test validated
    
    * refact
    
    * fix tests
    
    * db test
    
    * style
    
    * jsdoc fix
    
    * style
    
    * API side of workflow
    
    * config
    
    * clean up swagger
    
    * worker complete, need to test
    
    * style
    
    * final clean up
    
    * fix insert test
    
    * swagger
    
    * missing test in jobQueue

---
+++ tests/jobQueue/v1/bulkPostEvents.js
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
+
+/**
+ * tests/jobQueue/v1/bulkPostEvents.js
+ */
+
+const jobSetup = require('../../../jobQueue/setup');
+const jobQueue = jobSetup.jobQueue;
+const jobType = jobSetup.jobType;
+const bulkPostEventsJob = require('../../../worker/jobs/bulkPostEvents');
+const expect = require('chai').expect;
+const supertest = require('supertest');
+const api = supertest(require('../../../index').app);
+const tu = require('../../testUtils');
+const u = require('./utils');
+const constants = require('../../../api/v1/constants');
+const path = '/v1/events/bulk';
+
+describe('tests/jobQueue/v1/bulkPostEvents.js, ' +
+`api: POST using worker process ${path} >`, () => {
+  let token;
+
+  before(() => jobSetup.resetJobQueue());
+  after(() => jobSetup.resetJobQueue());
+
+  before((done) => {
+    tu.toggleOverride('enableWorkerProcess', true);
+    tu.toggleOverride('enableApiActivityLogs', false);
+    tu.toggleOverride('enableWorkerActivityLogs', false);
+    jobQueue.process(jobType.bulkPostEvents, bulkPostEventsJob);
+    tu.createToken()
+    .then((returnedToken) => {
+      token = returnedToken;
+      return done();
+    })
+    .catch((err) => done(err));
+  });
+
+  after(u.forceDelete);
+  after(tu.forceDeleteUser);
+  after(() => {
+    tu.toggleOverride('enableWorkerProcess', false);
+  });
+
+  it('OK, Returns ok status with the jobId', (done) => {
+    api.post(path)
+    .set('Authorization', token)
+    .send([
+      {
+        log: 'Something cool happened!',
+      },
+      {
+        log: 'Something even cooler happened!',
+      },
+    ])
+    .expect(constants.httpStatus.OK)
+    .expect((res) => {
+      expect(res.body.status).to.contain('OK');
+      expect(res.body).to.have.property('jobId');
+    })
+    .end(done);
+  });
+});
