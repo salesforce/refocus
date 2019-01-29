bf6709a2835424264b396a494e2fa07cd3051fac
Ian Goldstein <igoldstein@salesforce.com>
2019-01-29 15:35:03 -0800

    add debug to publisher trackStats

--- realtime/redisPublisher.js
+++ realtime/redisPublisher.js
@@ -10,6 +10,7 @@
  * ./realTime/redisPublisher.js
  */
 'use strict'; // eslint-disable-line strict
+const debug = require('debug')('refocus:publisher');
 const rtUtils = require('./utils');
 const config = require('../config');
 const client = require('../cache/redisCache').client;
@@ -32,6 +33,10 @@ const ONE = 1;
  * @param {Object} obj - The object being published
  */
 function trackStats(key, obj) {
+  if (key.indexOf('refocus.internal.realtime.subject.') >= 0) {
+    debug('redisPublisher.trackStats %s %o', key, obj);
+  }
+
   const elapsed = Date.now() - new Date(obj.updatedAt);
   rcache.hincrbyAsync(pubKeys.count, key, ONE)
     .catch((err) => {
@@ -41,7 +46,7 @@ function trackStats(key, obj) {
   rcache.hincrbyAsync(pubKeys.time, key, elapsed)
     .catch((err) => {
       console.error('redisPublisher.trackStats HINCRBY', pubKeys.time, key,
-        elapsed, err);
+        elapsed, err, obj);
     });
 } // trackStats
 
