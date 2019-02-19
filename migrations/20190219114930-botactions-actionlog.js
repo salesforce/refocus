87e91a5021c34be077bc7c47e01ec5b2fb8697ce
Brandon Murnane <bmurnane@salesforce.com>
2019-02-19 12:00:24 +0000

    added migration and test

---
+++ migrations/20190219114930-botactions-actionlog.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2019, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
+
+const constants = require('../db/constants');
+
+
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.addColumn(
+      'BotActions',
+      'actionLog',
+      {
+        type: Sequelize.STRING(constants.fieldlen.normalName),
+        allowNull: true,
+      }
+    ),
+
+  down: (queryInterface) =>
+    queryInterface.removeColumn(
+      'BotActions',
+      'actionLog'
+    ),
+};
