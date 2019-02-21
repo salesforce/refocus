da75528d3c47451a3119064e28479d033e166be3
Ian Goldstein <iamigo@users.noreply.github.com>
2019-02-21 12:44:23 -0800

    drop most of a sample's embedded subject attributes before publishing (#1117)

--- realtime/redisPublisher.js
+++ realtime/redisPublisher.js
@@ -175,6 +175,17 @@ function publishSample(sampleInst, subjectModel, event, aspectModel) {
     .then((sample) => {
       if (sample) {
         sample.absolutePath = sample.subject.absolutePath; // reqd for filtering
+
+        if (featureToggles.isFeatureEnabled('reduceSampleEventSize')) {
+          // Replace the sample.subject with subject name/absolutePath/tags only
+          const miniSubject = {
+            absolutePath: sample.subject.absolutePath,
+            name: sample.subject.name,
+            tags: sample.subject.tags,
+          };
+          sample.subject = miniSubject;
+        }
+
         publishObject(sample, eventType);
         return sample;
       }
