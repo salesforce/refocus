ba5fb647ca1237b9094ca29ea224c3e9e03ded94
Ian Goldstein <igoldstein@salesforce.com>
2019-04-13 18:49:25 -0700

    add logging and fix error handling

--- utils/whitelist.js
+++ utils/whitelist.js
@@ -6,17 +6,29 @@ module.exports = (req, res, next) => {
   let ipAddr = '';
   if (req.headers && req.headers['x-forwarded-for']) {
     ipAddr = req.headers['x-forwarded-for'];
+    console.log(`whitelist|x-forwarded-for|${ipAddr}`);
   } else if (req.connection && req.connection.remoteAddress) {
     ipAddr = req.connection.remoteAddress;
+    console.log(`whitelist|remoteAddress|${ipAddr}`);
   }
 
   const arr = ipAddr.split(',').map((a) => a.trim());
   const promises = arr.map((i) =>
     request.get(`${ipWhitelistApplication}/${path}/${i}`)
-      .then((_res) => Promise.resolve(_res.status === 200 && _res.body.allow)));
-  if (promises.every((p) => p)) {
+      .then((_res) => {
+        console.log('whitelist response', _res.status, _res.body);
+        Promise.resolve(_res.status === 200 && _res.body.allow)
+      }));
+  if (promises.every((p) => {
+    console.log(`whitelist|${p}`);
+    return p;
+  })) {
     return next();
   }
 
-  return next('Permission Denied');
+  const err = new Error('Access denied');
+  err.name = 'Unauthorized';
+  err.explanation = 'Unauthorized';
+  err.status = 401;
+  next(err);
 };
