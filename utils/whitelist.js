5fca3d083be97d8f456775bbebd93ea8082bfe1a
Ian Goldstein <igoldstein@salesforce.com>
2019-04-13 20:00:51 -0700

    add header

--- utils/whitelist.js
+++ utils/whitelist.js
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) 2019, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
+
+/**
+ * utils/whitelist.js
+ */
 const request = require('superagent');
 const ipWhitelistApplication = process.env.IP_WHITELIST_APPLICATION;
 const path = 'v1/verify';
@@ -6,15 +17,12 @@ module.exports = (req, res, next) => {
   let ipAddr = '';
   if (req.headers && req.headers['x-forwarded-for']) {
     ipAddr = req.headers['x-forwarded-for'];
-    console.log(`whitelist|x-forwarded-for|${ipAddr}`);
   } else if (req.connection && req.connection.remoteAddress) {
     ipAddr = req.connection.remoteAddress;
-    console.log(`whitelist|remoteAddress|${ipAddr}`);
   }
 
   request.get(`${ipWhitelistApplication}/${path}/${ipAddr}`)
     .then((_res) => {
-      console.log(`whitelist|response|${_res.status}|${_res.body}`);
       if (_res.status === 200 && _res.body.allow === true) {
         return next();
       };
@@ -23,7 +31,6 @@ module.exports = (req, res, next) => {
       err.name = 'Unauthorized';
       err.explanation = 'Unauthorized';
       err.status = 401;
-      console.log('whitelist|unauthorized', err);
       return next(err);
     })
     .catch(next);
