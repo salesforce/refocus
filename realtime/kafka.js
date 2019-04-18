f39fe3452a32533c9eca0ed078617d878aa1022e
Ian Goldstein <igoldstein@salesforce.com>
2019-04-18 11:50:29 -0700

    kafka for pubsub

--- realtime/kafka.js
+++ realtime/kafka.js
@@ -9,11 +9,9 @@ const producer = new Kafka.Producer({
 
 const topicName = 'perspectives';
 
-producer.connect();
-
 // Wait for the ready event before proceeding
-producer.on('ready', () => {
-  console.log('KAFKA producer is ready');
+producer.on('ready', (arg) => {
+  console.log('KAFKA producer is ready', JSON.stringify(arg));
   try {
     producer.produce(
       topicName, // topic name
@@ -26,16 +24,23 @@ producer.on('ready', () => {
     );
     console.log('KAFKA producer produced a message')
   } catch (err) {
-    console.error('A problem occurred when sending our message');
+    console.error('KAFKA A problem occurred when sending message');
     console.error(err);
   }
+
+  setInterval(producer.poll, 1000);
 });
 
 // Any errors we encounter, including connection errors
 producer.on('event.error', (err) => {
-  console.error('Error from producer');
-  console.error(err);
+  console.error('KAFKA Error from producer', JSON.stringify(err));
 });
+
+producer.on('disconnected', (arg) => {
+  console.log('KAFKA producer disconnected', JSON.stringify(arg));
+});
+
+producer.connect();
 
 module.exports = {
   init: () => {
