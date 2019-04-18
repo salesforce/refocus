b7c5ade422829a767fb3dace76db93050b78b7af
Ian Goldstein <igoldstein@salesforce.com>
2019-04-18 12:09:43 -0700

    kafka for pubsub

--- realtime/kafka.js
+++ realtime/kafka.js
@@ -1,50 +1,79 @@
-const Kafka = require('node-rdkafka');
+var Kafka = require('no-kafka');
 
 console.log('KAFKA entered realtime/kafka.js');
 
 const producer = new Kafka.Producer({
-  'debug' : 'all',
-  'metadata.broker.list': process.env.KAFKA_URL,
-  'dr_cb': true , // delivery report callback
-});
-
-const topicName = 'perspectives';
-
-// Wait for the ready event before proceeding
-producer.on('ready', (arg) => {
-  console.log('KAFKA producer is ready', JSON.stringify(arg));
-  try {
-    producer.produce(
-      topicName, // topic name
-      null, // partition
-      Buffer.from('Hello, World!'), // // Message to send. Must be a buffer
-      'key.a.b.c', // for keyed messages, we also specify the key
-      Date.now(), // timestamp
-      // you can send an opaque token here, which gets passed along
-      // to your delivery reports
-    );
-    console.log('KAFKA producer produced a message')
-  } catch (err) {
-    console.error('KAFKA A problem occurred when sending message');
-    console.error(err);
+  connectionString: process.env.KAFKA_URL,
+  ssl: {
+    cert: process.env.KAFKA_CLIENT_CERT || '.ssl/client.crt',
+    key: process.env.KAFKA_CLIENT_CERT_KEY || '.ssl/client.key'
   }
-
-  setInterval(producer.poll, 1000);
 });
 
-// Any errors we encounter, including connection errors
-producer.on('event.error', (err) => {
-  console.error('KAFKA Error from producer', JSON.stringify(err));
-});
+console.log('KAFKA PRODUCER', producer);
 
-producer.on('disconnected', (arg) => {
-  console.log('KAFKA producer disconnected', JSON.stringify(arg));
-});
+return producer.init().then(function (initResponse) {
+  console.log('KAFKA AFTER INIT', initResponse);
+  return producer.send({
+    topic: 'perspectives',
+    partition: 0,
+    message: {
+      key: 'a.b.c|def',
+      value: 'Hello!'
+    }
+  });
+})
+  .then(function (sendResponse) {
+    console.log('KAFKA AFTER SEND', sendResponse);
+  });
 
-producer.connect();
+// const Kafka = require('node-rdkafka');
+//
+// const producer = new Kafka.Producer({
+//   'debug' : 'all',
+//   'metadata.broker.list': process.env.KAFKA_URL,
+//   'dr_cb': true , // delivery report callback
+// });
+//
+// const topicName = 'perspectives';
+//
+// // Wait for the ready event before proceeding
+// producer.on('ready', (arg) => {
+//   console.log('KAFKA producer is ready', JSON.stringify(arg));
+//   try {
+//     producer.produce(
+//       topicName, // topic name
+//       null, // partition
+//       Buffer.from('Hello, World!'), // // Message to send. Must be a buffer
+//       'key.a.b.c', // for keyed messages, we also specify the key
+//       Date.now(), // timestamp
+//       // you can send an opaque token here, which gets passed along
+//       // to your delivery reports
+//     );
+//     console.log('KAFKA producer produced a message')
+//   } catch (err) {
+//     console.error('KAFKA A problem occurred when sending message');
+//     console.error(err);
+//   }
+//
+//   setInterval(producer.poll, 1000);
+// });
+//
+// // Any errors we encounter, including connection errors
+// producer.on('event.error', (err) => {
+//   console.error('KAFKA Error from producer', JSON.stringify(err));
+// });
+//
+// producer.on('disconnected', (arg) => {
+//   console.log('KAFKA producer disconnected', JSON.stringify(arg));
+// });
+//
+// producer.connect();
+//
+// module.exports = {
+//   init: () => {
+//
+//   },
+// }
 
-module.exports = {
-  init: () => {
-
-  },
-}
+const
