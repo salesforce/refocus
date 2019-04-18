d1f25fccb918ea1e2b4aa3b942eaa4a1f11c729a
Ian Goldstein <igoldstein@salesforce.com>
2019-04-18 11:26:50 -0700

    kafka for pubsub

--- realtime/kafka.js
+++ realtime/kafka.js
@@ -1,49 +1,40 @@
 const Kafka = require('node-rdkafka');
 
-//
-// const producer = new Kafka.Producer({
-//   'metadata.broker.list': 'localhost:9092',
-//   'dr_cb': true
-// });
-//
-// // Connect to the broker manually
-// producer.connect();
-//
-//
-// // Wait for the ready event before proceeding
-// producer.on('ready', function() {
-//   try {
-//     producer.produce(
-//       // Topic to send the message to
-//       'topic',
-//       // optionally we can manually specify a partition for the message
-//       // this defaults to -1 - which will use librdkafka's default partitioner (consistent random for keyed messages, random for unkeyed messages)
-//       null,
-//       // Message to send. Must be a buffer
-//       Buffer.from('Awesome message'),
-//       // for keyed messages, we also specify the key - note that this field is optional
-//       'Stormwind',
-//       // you can send a timestamp here. If your broker version supports it,
-//       // it will get added. Otherwise, we default to 0
-//       Date.now(),
-//       // you can send an opaque token here, which gets passed along
-//       // to your delivery reports
-//     );
-//   } catch (err) {
-//     console.error('A problem occurred when sending our message');
-//     console.error(err);
-//   }
-// });
-//
-// // Any errors we encounter, including connection errors
-// producer.on('event.error', function(err) {
-//   console.error('Error from producer');
-//   console.error(err);
-// })
-//
-// module.exports = {
-//   init: () => {
-//
-//   },
-// }
-//
+const producer = new Kafka.Producer({
+  'debug' : 'all',
+  'metadata.broker.list': process.env.KAFKA_URL,
+});
+
+const topicName = 'perspectives';
+
+producer.connect();
+
+// Wait for the ready event before proceeding
+producer.on('ready', () => {
+  try {
+    producer.produce(
+      topicName, // topic name
+      null, // partition
+      Buffer.from('Hello, World!'), // // Message to send. Must be a buffer
+      'key.a.b.c', // for keyed messages, we also specify the key
+      Date.now(), // timestamp
+      // you can send an opaque token here, which gets passed along
+      // to your delivery reports
+    );
+  } catch (err) {
+    console.error('A problem occurred when sending our message');
+    console.error(err);
+  }
+});
+
+// Any errors we encounter, including connection errors
+producer.on('event.error', (err) => {
+  console.error('Error from producer');
+  console.error(err);
+});
+
+module.exports = {
+  init: () => {
+
+  },
+}
