11871b58bee109dbdebf6e980131a76d069c64a8
Ian Goldstein <iamigo@users.noreply.github.com>
2019-06-26 15:00:39 -0700

    Update perspective page to dispatch the right hierarchy load event (#1188)

---
+++ tests/view/perspectives/reconstructV1Hierarchy.js
@@ -0,0 +1,98 @@
+/**
+ * Copyright (c) 2019, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
+
+/**
+ * tests/view/perspectives/reconstructV1Hierarchy.js
+ */
+const expect = require('chai').expect;
+const utils = require('../../../view/perspective/utils');
+const v1hierarchy = require('./v1hierarchy');
+const v2hierarchy = require('./v2hierarchy');
+
+describe('tests/view/perspectives/reconstructV1Hierarchy.js >', () => {
+  it('hierarchy already had v1 shape', () => {
+    const h = utils.reconstructV1Hierarchy(v1hierarchy);
+    expect(h).to.deep.equal(v1hierarchy);
+  });
+
+  it('reconstruct v1 hierarchy from v2 hierarchy', () => {
+    const h = utils.reconstructV1Hierarchy(v2hierarchy);
+    expect(h).to.deep.equal(v1hierarchy);
+  });
+
+  it('aspect name case insensitivity', () => {
+    const v2 = {
+      hierarchy: {
+        absolutePath: 'a',
+        children: [
+          {
+            absolutePath: 'a.b',
+            samples: [
+              {
+                name: 'a.b|a1',
+                value: '1',
+              }
+            ],
+          },
+          {
+            absolutePath: 'a.c',
+            samples: [
+              {
+                name: 'a.c|A1',
+                value: '2',
+              }
+            ],
+          },
+        ],
+      },
+      aspects: {
+        'A1': {
+          name: 'A1',
+          timeout: '10m',
+          id: '123-345',
+        }
+      },
+    };
+    const h = utils.reconstructV1Hierarchy(v2);
+    expect(h).to.deep.equal({
+      "absolutePath": "a",
+      "children": [
+        {
+          "absolutePath": "a.b",
+          "samples": [
+            {
+              "aspect": {
+                "id": "123-345",
+                "name": "A1",
+                "timeout": "10m",
+              },
+              "aspectId": "123-345",
+              "name": "a.b|a1",
+              "value": "1",
+            },
+          ],
+        },
+        {
+          "absolutePath": "a.c",
+          "samples": [
+            {
+              "aspect": {
+                "id": "123-345",
+                "name": "A1",
+                "timeout": "10m",
+              },
+              "aspectId": "123-345",
+              "name": "a.c|A1",
+              "value": "2",
+            },
+          ],
+        },
+      ],
+    });
+  });
+});
