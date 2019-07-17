6eab3139af6d3f240f0530389c35a19b46b2dedc
Deepanshu Gupta <guptadeepanshu25@gmail.com>
2019-07-17 13:31:56 -0700

    console log failing test output

--- tests/realtime/pubSubStats.js
+++ tests/realtime/pubSubStats.js
@@ -175,7 +175,7 @@ describe('tests/realtime/pubSubStats.js >', () => {
     });
   });
 
-  describe('log >', () => {
+  describe.only('log >', () => {
     let inspect;
 
     beforeEach(() => {
@@ -199,6 +199,7 @@ describe('tests/realtime/pubSubStats.js >', () => {
       const re1 = /info: activity=pubsub key=bye.world process=MyProcessName pubCount=3 pubTime=\d+ subCount=1 subTime=\d+ \n/; // jscs:ignore maximumLineLength
       const re2 = /info: activity=pubsub key=hello.world process=MyProcessName pubCount=1 pubTime=\d+ subCount=1 subTime=\d+ \n/; // jscs:ignore maximumLineLength
       expect(inspect.output).to.be.array;
+      console.log(inspect.output);
       expect(inspect.output).to.have.lengthOf(2);
       expect(inspect.output[0]).to.match(re1);
       expect(inspect.output[1]).to.match(re2);
