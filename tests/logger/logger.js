ec3f5ab42de2412e5d3bd689ecab6e8207ce4dee
Deepanshu Gupta <guptadeepanshu25@gmail.com>
2019-07-17 15:32:37 -0700

    remove only

--- tests/logger/logger.js
+++ tests/logger/logger.js
@@ -13,7 +13,7 @@ const kafkaConfig = require('../../config/kafkaConfig');
 const tu = require('../testUtils');
 
 
-describe.only('test/logger.js > ', () => {
+describe('test/logger.js > ', () => {
   it('Happy path:call producer with the right args,' +
   'call the init function and send', () => {
     tu.toggleOverride('kafkaLogging', true);
