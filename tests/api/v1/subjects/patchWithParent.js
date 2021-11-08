6d2fd80d6ac28a73a835e465b79095733b73638b
BrandonMurnane <bmurnane@salesforce.com>
2021-11-08 15:55:24 +0000

    Added test

--- tests/api/v1/subjects/patchWithParent.js
+++ tests/api/v1/subjects/patchWithParent.js
@@ -17,11 +17,13 @@ const tu = require('../../../testUtils');
 const u = require('./utils');
 const expect = require('chai').expect;
 const Subject = tu.db.Subject;
+const testUtils = require('../../../testUtils');
 const path = '/v1/subjects';
 const ZERO = 0;
 
 describe(`tests/api/v1/subjects/patchWithParent.js, PATCH ${path} >`, () => {
   let token;
+  let otherValidToken;
   const n0 = { name: `${tu.namePrefix}Canada`, isPublished: true };
   const n1 = { name: `${tu.namePrefix}Ontario`, isPublished: true };
   const n2 = { name: `${tu.namePrefix}Manitoba`, isPublished: true };
@@ -39,6 +41,7 @@ describe(`tests/api/v1/subjects/patchWithParent.js, PATCH ${path} >`, () => {
   let iRoot = ZERO;
 
   before((done) => {
+    testUtils.toggleOverride('validateParentWriters', true);
     tu.createToken()
     .then(() => tu.createUser('myUniqueUser'))
     .then((usr) => tu.createTokenFromUserName(usr.name))
@@ -190,6 +193,28 @@ describe(`tests/api/v1/subjects/patchWithParent.js, PATCH ${path} >`, () => {
         expect(res.body.absolutePath).to.equal(_root.name + '.' + NEW_NAME);
         done();
       });
+    });
+    it('on change parent, the parent is set by ' +
+        'parentId', (done) => {
+      const NEW_NAME = 'newName';
+
+      // use leaf subject
+      api.put(`${path}/${i0a}`)
+          .set('Authorization', otherValidToken)
+          .send({
+            name: NEW_NAME,
+            isPublished: false,
+            parentId: iRoot,
+          })
+          .expect(constants.httpStatus.BAD_REQUEST)
+          .end((err, res) => {
+            if (err) {
+              return done(err);
+            }
+
+            expect(res.body.errors[0].type).to.equal('ParentWriterRestricted');
+            done();
+          });
     });
   });
 
